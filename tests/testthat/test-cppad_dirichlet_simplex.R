deff6300bb94040605ea944f2af7b90cdb6613a0
Kassel Hingee <kassel.hingee@gmail.com>
2022-04-19 00:22:47 +1000

    more streamlined test still fails for simplex manifold

--- tests/testthat/test-cppad_dirichlet_simplex.R
+++ tests/testthat/test-cppad_dirichlet_simplex.R
@@ -2,31 +2,13 @@
 test_that("cppad-based Score2 estimate leads to a match for large number of observations", {
   smofun <- ptapesmo_simplex(c(1,1,1,3,3,3), 3)
   beta = c(-0.3, -0.1, 3)
-  n = 100000
+  n = 100
   set.seed(134)
   utabl <- MCMCpack::rdirichlet(n, beta+1)
 
-  smobj <- function(beta, utabl){
-    sc_perpt <- lapply(1:n, function(i){
-      scobj <- psmo(smofun, utabl[i, ], beta)
-      return(scobj)
-    })
-    scmo <- mean(unlist(sc_perpt))
-    return(scmo)
-  }
-
-  smobjgrad <- function(beta, utabl){
-    sc_perpt <- lapply(1:n, function(i){
-      scobj <- psmograd(smofun, utabl[i, ], beta)
-      return(scobj)
-    })
-    scmo <- colMeans(do.call(rbind, sc_perpt))
-    return(scmo)
-  }
-
   out <- optim(par = beta*0,
-               fn = function(beta){smobj(beta,utabl)},
-               gr = function(beta){smobjgrad(beta, utabl)},
+               fn = function(beta){smobj(smofun, beta,utabl)},
+               gr = function(beta){smobjgrad(smofun, beta, utabl)},
                method = "BFGS")
 
   expect_equal(out$par, beta, tolerance = 1E-3, ignore_attr = TRUE)
