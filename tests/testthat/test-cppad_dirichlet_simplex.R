cc9d57f976e81affeeba2c2a86837ff531ce3abe
Kassel Hingee <kassel.hingee@gmail.com>
2022-04-19 16:08:51 +1000

    recorded snapshot of current simplex calculations - to compare after the functions have been combined further

--- tests/testthat/test-cppad_dirichlet_simplex.R
+++ tests/testthat/test-cppad_dirichlet_simplex.R
@@ -13,3 +13,14 @@ test_that("cppad-based Score2 estimate leads to a match for large number of obse
 
   expect_equal(out$par, beta, tolerance = 1E-1, ignore_attr = TRUE)
 })
+
+test_that("Simplex estimates calculations are historically consistent", {
+  smofun <- ptapesmo_simplex(c(1,1,1,3,3,3), 3)
+  beta = c(-0.3, -0.1, 3)
+  n = 10
+  set.seed(134)
+  utabl <- MCMCpack::rdirichlet(n, beta+1)
+
+  expect_snapshot_value(smobj(smofun, beta + 0.5, utabl), style = "json2", tolerance = 1E-5)
+  expect_snapshot_value(smobjgrad(smofun, beta + 0.5, utabl), style = "json2", tolerance = 1E-5)
+})
