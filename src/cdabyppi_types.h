1d9f570b59111957ddb3e0454c2363ed2b34d0e8
Kassel Hingee <kassel.hingee@gmail.com>
2022-04-19 10:58:50 +1000

    types declaration added to git, it is has been used for a while

---
+++ src/cdabyppi_types.h
@@ -0,0 +1,13 @@
+# include <iostream>        // standard input/output
+# include <vector>          // standard vector
+# include <cppad/example/cppad_eigen.hpp>  //load eigen
+# include <cppad/cppad.hpp> // the CppAD package
+# include <Rcpp.h>
+using namespace Rcpp;
+
+typedef std::vector<double> svecd;
+typedef Eigen::Matrix<double, Eigen::Dynamic, 1> vecd; //a vector of a1type values
+typedef CppAD::AD<double> a1type;   // for first (outer) level of taping
+typedef Eigen::Matrix<a1type, Eigen::Dynamic, 1> veca1; //a vector of a1type values
+typedef CppAD::AD<a1type> a2type;  // for second (inner) level of taping
+typedef Eigen::Matrix<a2type, Eigen::Dynamic, 1> veca2;
