c2eacca0b2955cb1924653977541906712952a21
Kassel Hingee <kassel.hingee@gmail.com>
2022-04-19 11:01:28 +1000

    variable naming in tape llS updated

--- src/mycpp/dirichlet.cpp
+++ src/mycpp/dirichlet.cpp
@@ -26,27 +26,27 @@ namespace { // begin the empty namespace
 
     // define a function that tapes the above function
     template <class a1type>
-      CppAD::ADFun<a1type> tapellS(Eigen::Matrix<a1type, Eigen::Dynamic, 1> xbeta){
+      CppAD::ADFun<a1type> tapellS(Eigen::Matrix<a1type, Eigen::Dynamic, 1> zbeta){
       typedef CppAD::AD<a1type> a2type;  // for second (inner) level of taping
       size_t n = 3;                  // number of dimensions
 
-      Eigen::Matrix<a1type, Eigen::Dynamic, 1> a(n); // vector of exponents in the outer type
+      Eigen::Matrix<a1type, Eigen::Dynamic, 1> beta(n); // vector of exponents in the outer type
       //declare dummy internal level of taping variables:
-      Eigen::Matrix<a2type, Eigen::Dynamic, 1> ax(n); // vector of domain space variables
+      Eigen::Matrix<a2type, Eigen::Dynamic, 1> z(n); // vector of domain space variables
       for(int i = 0; i < n; i++){
-         a[i] = xbeta[i + n];
-         ax[i] = 3.;
+         beta[i] = zbeta[i + n];
+         z[i] = zbeta[i];
       }
 
       //tape relationship between x and log-likelihood
-      CppAD::Independent(ax);
+      CppAD::Independent(z);
 
       // range space vector
       size_t m = 1;               // number of ranges space variables
-      Eigen::Matrix<a2type, Eigen::Dynamic, 1> ay(m); // vector of ranges space variables
-      ay[0] = llS(a, ax);     // record operations that compute ay[0]
+      Eigen::Matrix<a2type, Eigen::Dynamic, 1> y(m); // vector of ranges space variables
+      y[0] = llS(beta, z);     // record operations that compute ay[0]
       CppAD::ADFun<a1type> f;  //copying the change_parameter example, a1type is used in constructing f, even though the input and outputs to f are both a2type.
-      f.Dependent(ax, ay);
+      f.Dependent(z, y);
       return(f);
   }
 
