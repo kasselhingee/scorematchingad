5710f82acefa2ec0bb66dcb5511564750d4e57b3
Kassel Hingee <kassel.hingee@gmail.com>
2022-04-22 15:12:12 +1000

    turned ll functions into templated functions

--- src/mycpp/dirichlet.cpp
+++ src/mycpp/dirichlet.cpp
@@ -1,23 +1,26 @@
 
 namespace { // begin the empty namespace
 
-    a1type ll_dirichlet(const veca1 &beta,
-	       const veca1 &u)
+    template <class T>
+    T ll_dirichlet(const Eigen::Matrix<T, Eigen::Dynamic, 1> &beta,
+	       const Eigen::Matrix<T, Eigen::Dynamic, 1> &u)
     {   size_t d  = u.size();
-        a1type y(0.);  // initialize summation
+        T y(0.);  // initialize summation
         for(size_t i = 0; i < d; i++)
         {   y   += beta[i] * log(u[i]);
         }
         return y;
     }
 
-    a1type ll_ppi(const veca1 &beta, const veca1 & u){
+    template <class T>
+    T ll_ppi(const Eigen::Matrix<T, Eigen::Dynamic, 1> &beta,
+             const Eigen::Matrix<T, Eigen::Dynamic, 1> & u){
         size_t d  = u.size();
         //assume the parameter vector beta is encoded as:
         //c(diag(ALs), ALs[upper.tri(ALs)], bL, betafordirichlet)
-        mata1 Amat(d, d);
+        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Amat(d, d);
         Amat.setZero();
-        mata1 ALmat(d-1, d-1);
+        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> ALmat(d-1, d-1);
         ALmat.diagonal() = beta.block(0,0,d-1,1);
         //the upper triangle has (d-1)-1 rows, the rows have 1 - ()d-2) elements. Arithmetic series:
         //(d-2) * (2 + d - 3) / 2 = (d-2) * (d-1)/2
@@ -25,13 +28,13 @@ namespace { // begin the empty namespace
         ALmat.triangularView<Eigen::Upper>() = beta.block(d-1, 0, upptrisize, 1);
         ALmat.triangularView<Eigen::Lower>() = beta.block(d-1, 0, upptrisize, 1);
         Amat.block(0,0,d-1,d-1) = ALmat;
-        veca1 bvec(d);
+        Eigen::Matrix<T, Eigen::Dynamic, 1> bvec(d);
         bvec.setZero();
         bvec.block(0,0,d-1,1) = beta.block(d-1 + upptrisize, 0, d-1, 1);
 
-        a1type y(0.);  // initialize summation
+        T y(0.);  // initialize summation
         y += u.transpose()*Amat*u;
-        y += b.transpose()*u;
+        y += bvec.transpose()*u;
         //dirichlet component
         for(size_t i = 0; i < d; i++)
         {   y   += beta[i] * log(u[i]);
