051d6874095bf05b31219f269bfa60a4a9e3f1fe
Kassel Hingee <kassel.hingee@gmail.com>
2022-04-26 17:18:26 +1000

    grad of ppi ll on sphere appears to be correct inside psmo according to print statements. So makes me thing the objective value is unstable

--- tests/testthat/test-cppad_ppi.R
+++ tests/testthat/test-cppad_ppi.R
@@ -47,40 +47,34 @@ test_that("ppi works when AL and bL is zero and p = 4", {
 
 test_that("ppi with minsq weights match estimator1 for p = 4", {
   acut = 1
-  p=4
   # smofun <- ptapesmo(c(1,1,1,1,3,3,3,3,3,3,3,3,3), p, llname = "ppi", manifoldname = "sphere", "minsq", acut = acut) #tape of the score function
   #sample size
   n=1000
 
   #parameters for the PPI model
-  muL=matrix(0,p-1,1)
-  muL[1:sum(p,-1)]=0.12
-  aa=matrix(1/500,p-1,1)
-  D=diag(as.vector(aa))
-  SigA=D
-  SigA[1,1]=SigA[1,1]*2
-  cor=0.5
-  SigA[1,2]=cor*sqrt(SigA[1,1]*SigA[2,2])
-  SigA[2,1]=SigA[1,2]
-  ALs=-0.5*solve(SigA)
-  bL=solve(SigA)%*%muL
-  beta0=matrix(-0.3,p,1)
-  beta0[p]=-0.5
-  theta <- toPPIparamvec(ALs, bL, beta0)
+  beta = c(-0.3, -0.2, -0.1, 3)
+  p = length(beta)
+  ALs = matrix(0, nrow = p-1, ncol = p-1)
+  bL = matrix(0, nrow = p-1, ncol = 1)
+  bL[1, 1] = 1
+  theta <- toPPIparamvec(ALs, bL, beta)
 
   set.seed(134)
   utabl <- cdabyppi:::rhybrid(n,p,beta0,ALs,bL,4)$samp3
-  smofun <- ptapesmo(c(utabl[2, ], 1:length(theta)), p, llname = "ppi", manifoldname = "sphere", "minsq", acut = acut) #tape of the score function
+  u <- utabl[2, ]
+  smofun <- ptapesmo(c(u, 1:length(theta)), p, llname = "ppi", manifoldname = "sphere", "minsq", acut = acut) #tape of the score function
 
-  psmo(smofun, utabl[2, ], theta) #very strange that the 4th component of the gradient of the ll on the sphere is zero
-  # deriv of log ppi ll on sphere wrt z[4] is:
-  # (1 + 2beta[4]) / z[4] + 0 + 0 which is zero because beta0[4] is -0.5
+  psmo(smofun, u, theta) #very strange that the 4th component of the gradient of the ll on the sphere is zero
+  # deriv of log ppi ll on sphere wrt z[1] is:
+  # (1 + 2beta[1]) / z[1] + 0 + 2*z[1]
+  (1 + 2 * beta[1]) / sqrt(u[1]) + 2 * sqrt(u[1])
+  (1 + 2 * beta[4]) / sqrt(u[4])
   lltape <- ptapell(p, length(theta), llname = "ppi")
   pJacobian(lltape, utabl[1,], theta)
   # There are better optimisers than below: John Nash at https://www.r-bloggers.com/2016/11/why-optim-is-out-of-date/)
-  out <- optim(par = theta*0,
+  out <- optim(par = theta,
                fn = function(theta){smobj(smofun, theta, utabl)},
-               # gr = function(theta){smobjgrad(smofun, theta, utabl)},
+               gr = function(theta){smobjgrad(smofun, theta, utabl)},
                method = "BFGS")
 
   # memoisation could be used to avoid calling the smobj function again for gradient computation
