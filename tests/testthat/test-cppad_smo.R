781aa06abece100465015a7f35ad60b2a07d6c23
Kassel Hingee <kassel.hingee@gmail.com>
2022-04-27 17:03:59 +1000

    it was the value of acut that was the problem - a larger value is better!

--- tests/testthat/test-cppad_smo.R
+++ tests/testthat/test-cppad_smo.R
@@ -75,31 +75,26 @@ test_that("minsq weights match estimator2 for d = 4", {
 })
 
 test_that("fixed beta[p] with minsq weights match true value", {
-  acut = 0.01
+  acut = 0.1
   beta = c(-0.2, -0.1, 3)
-  n = 1000
+  n = 10000
   set.seed(134)
   utabl <- MCMCpack::rdirichlet(n, beta+1)
 
   psphere <- pmanifold("sphere")
-  pdir <- ptapell(c(0.1,0.1,0.1), c(3,3,3), llname = "dirichlet", psphere, fixedtheta = c(FALSE, FALSE, TRUE))
+  pdir <- ptapell(c(0.1,0.1,0.1), c(1,2,3), llname = "dirichlet", psphere, fixedtheta = c(FALSE, FALSE, TRUE))
   smofun <- ptapesmo(c(0.1,0.1,0.1),
                      c(1, 2),
                      pll = pdir,
                      pman = psphere, "minsq", acut = acut) #tape of the score function
 
-  pForward0(pdir, utabl[1,], beta[-3] * 0)
-  pForward0(smofun, beta[-3], utabl[1, ])
-
   # There are better optimisers than below: John Nash at https://www.r-bloggers.com/2017/11/why-optim-is-out-of-date/)
-  out <- optim(par = beta[-3]*0,
+  out <- optim(par = beta[-3] * 0,
                fn = function(beta){smobj(smofun, beta, utabl)},
                gr = function(beta){smobjgrad(smofun, beta, utabl)},
                method = "BFGS")
 
-  val <- smobj(smofun, beta[-3], utabl)
-  val2 <- smobj(smofun, out$par, utabl)
-  val3 <- smobj(smofun, beta[-3] * 0, utabl)
-
-  expect_equal(out$par, beta[-3], tolerance = 1E-3, ignore_attr = TRUE)
+  directestimate <- estimator1_dir(utabl, acut)
+  expect_equal(out$par, directestimate[-3], tolerance = 0.05, ignore_attr = TRUE) #tolerance is usually relative
+  expect_equal(out$par, beta[-3], tolerance = 0.1, ignore_attr = TRUE) #tolerance is usually relative
 })
