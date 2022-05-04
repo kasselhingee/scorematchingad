61a0e8039fff3ec20e82d90cd18606b99fd77ea6
Kassel Hingee <kassel.hingee@gmail.com>
2022-05-04 15:27:33 +1000

    moving some tests around

--- tests/testthat/test-cppad-dirichlet.R
+++ tests/testthat/test-cppad-dirichlet.R
@@ -98,3 +98,35 @@ test_that("fixed beta[p] with minsq weights match true value", {
   expect_equal(out$par, directestimate[-3], tolerance = 0.05, ignore_attr = TRUE) #tolerance is usually relative
   expect_equal(out$par, beta[-3], tolerance = 0.1, ignore_attr = TRUE) #tolerance is usually relative
 })
+
+
+test_that("cppad-based Score2 estimate leads to a match for large number of observations", {
+  psimplex <- pmanifold("simplex")
+  pdir <- ptapell(rep(1,3), rep(3, 3), "dirichlet", psimplex, fixedtheta = rep(FALSE, 3), verbose = FALSE)
+  smofun <- ptapesmo(rep(1,3), rep(3, 3), pdir, psimplex, weightname = "prodsq", acut = 0.01, verbose = FALSE)
+  beta = c(-0.3, -0.1, 3)
+  n = 1000
+  set.seed(134)
+  utabl <- MCMCpack::rdirichlet(n, beta+1)
+
+  out <- optim(par = beta*0,
+               fn = function(beta){smobj(smofun, beta,utabl)},
+               gr = function(beta){smobjgrad(smofun, beta, utabl)},
+               method = "BFGS")
+
+  expect_equal(out$par, beta, tolerance = 1E-1, ignore_attr = TRUE)
+})
+
+test_that("Simplex calculations are historically consistent", {
+  psimplex <- pmanifold("simplex")
+  pdir <- ptapell(rep(1,3), rep(3, 3), "dirichlet", psimplex, fixedtheta = rep(FALSE, 3), verbose = FALSE)
+  smofun <- ptapesmo(rep(1,3), rep(3, 3), pdir, psimplex, weightname = "prodsq", acut = 1, verbose = FALSE)
+
+  beta = c(-0.3, -0.1, 3)
+  n = 10
+  set.seed(134)
+  utabl <- MCMCpack::rdirichlet(n, beta+1)
+
+  expect_snapshot_value(smobj(smofun, beta + 0.5, utabl), style = "json2", tolerance = 1E-5)
+  expect_snapshot_value(smobjgrad(smofun, beta + 0.5, utabl), style = "json2", tolerance = 1E-5)
+})
