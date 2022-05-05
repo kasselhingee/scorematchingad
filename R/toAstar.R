5f7389bc6b53011c252cb71ae80c14dea619f706
Kassel Hingee <kassel.hingee@gmail.com>
2022-05-05 18:33:38 +1000

    building up capacity to generate A* matrices

--- R/toAstar.R
+++ R/toAstar.R
@@ -4,9 +4,10 @@
 #' @param ALs The block of A that is non zero (which is the first p-1th row and column)
 #' @param bL The first p-1th elements of b (the final element of b must be zero by definition)
 #' @examples
+#' AL = matrix(c(-166, 117, 117, -333), nrow = 2, ncol = 2)
+#' bL = rep(1, 2)
+#' toAstar(AL, bL)
 #'
-#'
-#' samp <- c(0.007, 0.05, 1 - 0.007 - 0.05)
 #' @export
 toAstar <- function(ALs, bL){
   p <- nrow(ALs) + 1
@@ -14,4 +15,9 @@ toAstar <- function(ALs, bL){
   A[1:(p-1), 1:(p-1)] <- ALs
   Astar <- A + matrix(c(bL, 0), byrow = TRUE, nrow = p, ncol = p) # each row of A element wise addition to b, gives Astar, as defined in eqn 2
   return(Astar)
+}
+
+fromAstar <- function(Astar){
+  stopifnot(isSymmetric.matrix(Astar))
+
 }
