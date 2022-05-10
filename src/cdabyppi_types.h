d0f2aa8dfb1adc98aa5366d080a74ed6a724c26e
Kassel Hingee <kassel.hingee@gmail.com>
2022-05-10 15:37:16 +1000

    moved simplexman definition into the manifold_simplex.cpp file and everything still works

--- src/cdabyppi_types.h
+++ src/cdabyppi_types.h
@@ -37,46 +37,4 @@ struct manifold { //exactly like a class, but with default public members https:
   manifold(){};
 };
 
-template <typename T>
-struct simplexman : public manifold<T> {
-  Eigen::Matrix<T, Eigen::Dynamic, 1> toM(const Eigen::Matrix<T, Eigen::Dynamic, 1> &x){
-    Eigen::Matrix<T, Eigen::Dynamic, 1> out(x.size());
-    out = x;
-    return(out);
-  }
 
-  Eigen::Matrix<T, Eigen::Dynamic, 1> fromM(const Eigen::Matrix<T, Eigen::Dynamic, 1> &x){
-    Eigen::Matrix<T, Eigen::Dynamic, 1> out(x.size());
-    out = x;
-    return(out);
-  }
-
-  T logdetJfromM(const Eigen::Matrix<T, Eigen::Dynamic, 1> &z){
-    T out;
-    out = 0.;
-    return(out);
-  }
-
-
-  // manifold tangent-plane projection matrix P (for isometric(?) embeddings this is closely related to the manifold metric
-  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Pmatfun(const Eigen::Matrix<T, Eigen::Dynamic, 1> &x){
-    int n = x.size();
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Pmat(n, n);
-    Eigen::Matrix<T, Eigen::Dynamic, 1> ones(n);
-    ones.setOnes();
-    double nd = n;
-    Pmat = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Identity(n,n) - (ones*ones.transpose()/nd);
-    return(Pmat);
-  }
-
-  //partial derivative of the tangent-plane projection matrix
-  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> dPmatfun(const Eigen::Matrix<T, Eigen::Dynamic, 1> &x, const int &d){
-    int n = x.size();
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> bvx(n, n);
-    bvx.setZero();
-    return(bvx);
-  }
-
-  ~simplexman(){};
-  simplexman(){};
-};
--- src/mycpp/manifold_simplex.cpp
+++ src/mycpp/manifold_simplex.cpp
@@ -1,72 +1,67 @@
-// code for various tools for the simplex in full Euclidean space
-namespace simplex { // begin the empty namespace
-
-  template <class Type>
-  Eigen::Matrix<Type, Eigen::Dynamic, 1> toM(const Eigen::Matrix<Type, Eigen::Dynamic, 1> &x){
-     Eigen::Matrix<Type, Eigen::Dynamic, 1> out(x.size());
-     out = x;
-     return(out);
+template <typename T>
+struct simplexman : public manifold<T> {
+  Eigen::Matrix<T, Eigen::Dynamic, 1> toM(const Eigen::Matrix<T, Eigen::Dynamic, 1> &x){
+    Eigen::Matrix<T, Eigen::Dynamic, 1> out(x.size());
+    out = x;
+    return(out);
   }
 
-  template <class Type>
-  Eigen::Matrix<Type, Eigen::Dynamic, 1> fromM(const Eigen::Matrix<Type, Eigen::Dynamic, 1> &x){
-     Eigen::Matrix<Type, Eigen::Dynamic, 1> out(x.size());
-     out = x;
-     return(out);
+  Eigen::Matrix<T, Eigen::Dynamic, 1> fromM(const Eigen::Matrix<T, Eigen::Dynamic, 1> &x){
+    Eigen::Matrix<T, Eigen::Dynamic, 1> out(x.size());
+    out = x;
+    return(out);
   }
 
-  template <class Type>
-  Type logdetJ_fromM(const Eigen::Matrix<Type, Eigen::Dynamic, 1> &z){
-     Type out;
-     out = 0.;
-     return(out);
+  T logdetJfromM(const Eigen::Matrix<T, Eigen::Dynamic, 1> &z){
+    T out;
+    out = 0.;
+    return(out);
   }
 
 
   // manifold tangent-plane projection matrix P (for isometric(?) embeddings this is closely related to the manifold metric
-  template <class Type>
-  Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic> Pmat_M(const Eigen::Matrix<Type, Eigen::Dynamic, 1> &x){
+  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Pmatfun(const Eigen::Matrix<T, Eigen::Dynamic, 1> &x){
     int n = x.size();
-    Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic> Pmat(n, n);
-    Eigen::Matrix<Type, Eigen::Dynamic, 1> ones(n);
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Pmat(n, n);
+    Eigen::Matrix<T, Eigen::Dynamic, 1> ones(n);
     ones.setOnes();
     double nd = n;
-    Pmat = Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic>::Identity(n,n) - (ones*ones.transpose()/nd);
+    Pmat = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Identity(n,n) - (ones*ones.transpose()/nd);
     return(Pmat);
   }
 
   //partial derivative of the tangent-plane projection matrix
-  template <class Type>
-  Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic> dPmat_M(const Eigen::Matrix<Type, Eigen::Dynamic, 1> &x, const int &d){
+  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> dPmatfun(const Eigen::Matrix<T, Eigen::Dynamic, 1> &x, const int &d){
     int n = x.size();
-    Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic> bvx(n, n);
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> bvx(n, n);
     bvx.setZero();
     return(bvx);
   }
 
+  ~simplexman(){};
+  simplexman(){};
 
 ////////////////////APPROX HELPERS/////////////////////
   //automatically choose approximation centre
-  template <class Type>
-  Eigen::Matrix<Type, Eigen::Dynamic, 1> taylorapprox_bdry(
-		  CppAD::ADFun<Type> &f,
+  Eigen::Matrix<T, Eigen::Dynamic, 1> taylorapprox_bdry(
+		  CppAD::ADFun<T> &f,
 		  const size_t order,
-		  const Eigen::Matrix<Type, Eigen::Dynamic, 1> xbeta,
+		  const Eigen::Matrix<T, Eigen::Dynamic, 1> xbeta,
 		  double shiftsize=1E-5){
-     Eigen::Matrix<Type, Eigen::Dynamic, 1> x(xbeta.size() / 2);
+     Eigen::Matrix<T, Eigen::Dynamic, 1> x(xbeta.size() / 2);
      x << xbeta.block(0,0,x.size(), 1);
-     Eigen::Matrix<Type, Eigen::Dynamic, 1> shiftdir(x.size());
+     Eigen::Matrix<T, Eigen::Dynamic, 1> shiftdir(x.size());
      shiftdir.setOnes();
      shiftdir *= -1 * shiftsize;
      shiftdir = Pmat_M(x) * shiftdir;
 
-     Eigen::Matrix<Type, Eigen::Dynamic, 1> centre(xbeta.size());
+     Eigen::Matrix<T, Eigen::Dynamic, 1> centre(xbeta.size());
      centre = xbeta;
      centre.block(0,0,x.size(), 1) << x + shiftdir;
      std::cout << "Approximation centre is:" << shiftdir.transpose() << std::endl;
-     Eigen::Matrix<Type, Eigen::Dynamic, 1> out(0);
+     Eigen::Matrix<T, Eigen::Dynamic, 1> out(0);
      out = taylorapprox(f, centre, order, xbeta);
      return(out);
   }
 
-}
+};
