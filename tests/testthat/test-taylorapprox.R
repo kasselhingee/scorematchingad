885fbe44363663a8b938bb5944cd378e06ad30a8
Kassel Hingee <kassel.hingee@gmail.com>
2022-05-16 14:51:05 +1000

    testing of raw taylor approximation is working

--- tests/testthat/test-taylorapprox.R
+++ tests/testthat/test-taylorapprox.R
@@ -2,6 +2,35 @@ test_that("Raw taylor approximation works on single sample points", {
   set.seed(123)
   m = sec2_3model(1)
   middleofsimplex <- rep(1/m$p, m$p)
+  shiftdir <- middleofsimplex - m$sample
+  shiftdir <- shiftdir / sqrt(sum(shiftdir^2)) #make a unit vector
+  shiftsize = 1E-2
+  approxcentre <- m$sample + shiftsize * shiftdir
+  approxcentre
+  # normalise
+  approxcentre <- ((m$sample * (99/100) + middleofsimplex * (1/100)))
+
+  psimplex <- pmanifold("simplex") #because above ppill_r is for the simplex
+  lltape <- ptapell(m$sample, m$theta, llname = "ppi", pman = psimplex, fixedtheta = rep(FALSE, length(m$theta)), verbose = FALSE)
+  expect_equal(pForward0(lltape, m$sample, m$theta), pTaylorApprox(lltape, m$sample, approxcentre, m$theta, 100))
+
+  smoppi <- ptapesmo(c(0.1,0.1,0.1), m$theta, pll = lltape, pman = psimplex, "minsq", acut = 0.1, verbose = FALSE) #tape of the score function
+  smoppi_u <- swapDynamic(smoppi, m$sample, m$theta)
+  expect_equal(pForward0(smoppi, m$theta, m$sample), pForward0(smoppi_u, m$sample, m$theta)) #sanity check swap of dynamic param and independent params
+  expect_equal(pForward0(smoppi_u, m$sample, m$theta),
+               pTaylorApprox(smoppi_u, m$sample, approxcentre, m$theta, 100))
+
+  # test a lower order approximation
+  expect_equal(pForward0(smoppi_u, m$sample, m$theta),
+               pTaylorApprox(smoppi_u, m$sample, approxcentre, m$theta, 10),
+               tolerance = 1E-4)
+  # it is much faster, but not as accurate
+})
+
+test_that("Auto approx centre with taylor approximation works on single sample points", {
+  set.seed(123)
+  m = sec2_3model(1)
+  middleofsimplex <- rep(1/m$p, m$p)
   approxcentre <- ((m$sample * (99/100) + middleofsimplex * (1/100)))
 
   psimplex <- pmanifold("simplex") #because above ppill_r is for the simplex
