ce8b347e3b489be761e342873bc6f90adab99945
Kassel Hingee <kassel.hingee@gmail.com>
2022-06-01 09:26:29 +1000

    bug fixes and using buildsmotapes for the vMF() collection

--- R/vMF.R
+++ R/vMF.R
@@ -1,16 +1,20 @@
 #' @title Score matching estimate for the von-Mises Fisher distribution
 #' @param sample Samples from the vMF distribution in cartesian Rd coordinates. Each row is a measurement.
+#' @param km Optional for the `smfull` method. A vector of same length as the dimension, representing the elements of the concentraction * Fisher direction vector.
+#' If supplied, the non-NA elements are fixed.
 #' @param control Control parameters passed to `Rcgmin::Rcgmin()`
+#' @param method Either `Mardia` for the hybrid score matching estimate from Mardia et al 2016 or `smfull` for a full score matching estimate.
 #' @examples
 #' sample <- movMF::rmovMF(100, 3 * c(1, -1) / sqrt(2))
 #' vMF(sample, "smfull")
 #' vMF(sample, "Mardia")
 #' @export
-vMF <- function(sample, method = "smfull", control = list(tol = 1E-20)){
+vMF <- function(sample, km = NULL, method = "smfull", control = list(tol = 1E-20)){
   out <- NULL
-  if (method == "smfull"){out <- vMF_full(sample, control = list(tol = 1E-20))}
+  if (method == "smfull"){out <- vMF_full(sample, km = km, control = control)}
   if (method == "Mardia"){
-    out <- vMF_Mardia(sample, control = list(tol = 1E-20))
+    stopifnot(is.null(km))
+    out <- vMF_Mardia(sample, control = control)
   }
   return(out)
 }
@@ -18,36 +22,51 @@ vMF <- function(sample, method = "smfull", control = list(tol = 1E-20)){
 vMF_Mardia <- function(sample, control = list(tol = 1E-20)){
   mu <- colMeans(sample)
   mu <- mu/sqrt(sum(mu^2))
-  R <- Directional::rotation(mu, c(1, rep(0, length(mu) - 1)))
-  samplestd <- t(R %*% t(sample))
+  Rtrans <- Directional::rotation(mu, c(1, rep(0, length(mu) - 1)))
+  samplestd <- sample %*% t(Rtrans)
   # check: mustd <- colMeans(samplestd); mustd <- mustd / sqrt(sum(mustd^2))
+
   # do estimate, where all but the first component of theta are fixed at zero
   # because kappa * e1 = (kappa, 0, 0, 0, ...)
-  pman <- pmanifold("Snative")
   p <- ncol(sample)
-  lltape <- ptapell(rep(1, p) / p, c(1, rep(0, p - 1)), llname = "vMF", pman,
-                    fixedtheta = c(FALSE, rep(TRUE, p - 1)), verbose = FALSE)
-  smotape<- ptapesmo(rep(1, p) / p, 1,
-                     pll = lltape, pman = pman, "ones", acut = 1, verbose = FALSE) #tape of the score function
-  out <- smest(smotape, 1, sample, control = control)
+  intheta <- c(NA, rep(0, p - 1))
+  tapes <- buildsmotape("Snative", "vMF",
+                        rep(1, p)/sqrt(p), intheta,
+                        weightname = "ones",
+                        verbose = FALSE)
+  out <- smest(tapes$smotape, rep(0.1, sum(is.na(intheta))), samplestd, control = control)
   return(list(
-    kappa = out$par,
-    mu = mu,
+    k = out$par,
+    m = mu,
+    km = out$par * mu,
+    SE = list(k = out$SE),
     sminfo = out
   ))
 }
 
-vMF_full <- function(sample, control = list(tol = 1E-20)){
-  pman <- pmanifold("Snative")
+vMF_full <- function(sample, km = NULL, control = list(tol = 1E-20)){
   p <- ncol(sample)
-  lltape <- ptapell(rep(1, p) / p, rep(0.1, p), llname = "vMF", pman,
-                    fixedtheta = rep(FALSE, p), verbose = FALSE)
-  smotape<- ptapesmo(rep(1, p) / p, rep(0.1, p),
-                     pll = lltape, pman = pman, "ones", acut = 1, verbose = FALSE) #tape of the score function
-  out <- smest(smotape, rep(0.1, p), sample, control = control)
+  if (is.null(km)){
+    km <- rep(NA, p)
+  }
+  stopifnot(length(km) == p)
+
+  intheta <- km
+  tapes <- buildsmotape("Snative", "vMF",
+                        rep(1, p)/sqrt(p), intheta,
+                        weightname = "ones",
+                        verbose = FALSE)
+  out <- smest(tapes$smotape, rep(0.1, sum(is.na(intheta))), sample, control = control)
+  theta <- intheta
+  theta[is.na(intheta)] <- out$par
+
+  SE <- intheta * 0
+  SE[is.na(intheta)] <- out$SE
   return(list(
-    kappa = sqrt(sum(out$par^2)),
-    mu = out$par / sqrt(sum(out$par^2)),
+    km = theta,
+    k = sqrt(sum(theta^2)),
+    m = theta / sqrt(sum(theta^2)),
+    SE = list(km = SE),
     sminfo = out
   ))
 }
--- tests/testthat/test-vMF.R
+++ tests/testthat/test-vMF.R
@@ -4,22 +4,76 @@ test_that("von-Mises Fisher likelihood runs and fits", {
   sample <- movMF::rmovMF(1000, theta)
   thetatape <- c(1, -1)
 
-  pman <- pmanifold("Snative")
-  lltape <- ptapell(sample[1,], thetatape, llname = "vMF", pman,
-                  fixedtheta = rep(FALSE, length(theta)), verbose = FALSE)
-  expect_equal(pForward0(lltape, sample[1, ], theta), sum(sample[1, ]  * theta)) ## very important to check a tape
-
-  smotape<- ptapesmo(sample[1,], thetatape,
-                     pll = lltape, pman = pman, "ones", acut = 1, verbose = FALSE) #tape of the score function
-
-  out <- smest(smotape, thetatape, sample, control = list(tol = 1E-10))
+  p <- length(theta)
+  intheta <- c(NA, rep(0, p - 1))
+  tapes <- buildsmotape("Snative", "vMF",
+                        rep(1, p)/sqrt(p), rep(NA, p),
+                        weightname = "ones",
+                        verbose = FALSE)
+  expect_equal(pForward0(tapes$lltape, sample[1, ], theta), sum(sample[1, ]  * theta)) ## very important to check a tape
+  out <- smest(tapes$smotape, thetatape, sample, control = list(tol = 1E-10))
   cdabyppi:::expect_lt_v(abs(out$par - theta), 3 * out$SE)
 })
 
-test_that("vMF() function works", {
+test_that("vMF_Mardia() function works on data around north pole", {
   set.seed(123)
-  theta <-  3 * c(1, -1) / sqrt(2)
-  sample <- movMF::rmovMF(100, theta)
-  out <- vMF(sample)
-  cdabyppi:::expect_lt_v(abs(out$par - theta), 3 * out$SE)
+  k <- 3
+  m <- c(1, 0)
+  km <-  k * m
+  sample <- movMF::rmovMF(1000, km)
+  out <- vMF(sample, method = "Mardia")
+  expect_equal(out$m , m, tolerance = 1E-1) #moment estimate part
+  cdabyppi:::expect_lt_v(abs(out$k - k), 3 * out$SE$k)
+})
+
+test_that("vMF_Mardia() function works for data centred elsewhere", {
+  set.seed(123)
+  k <- 3
+  m <- c(1, -1) / sqrt(2)
+  km <-  k * m
+  sample <- movMF::rmovMF(1000, km)
+  out <- vMF(sample, method = "Mardia")
+  expect_equal(out$m , m, tolerance = 1E-1) #moment estimate part
+  cdabyppi:::expect_lt_v(abs(out$k - k), 3 * out$SE$k)
+})
+
+test_that("vMF_Full() function works", {
+  set.seed(123)
+  k <- 3
+  m <- c(1, -1) / sqrt(2)
+  km <-  k * m
+  sample <- movMF::rmovMF(1000, km)
+  out <- vMF(sample, method = "smfull")
+  cdabyppi:::expect_lt_v(abs(out$km - km), 3 * out$SE$km)
+
+  #with a fixed component
+  inkm <- km
+  inkm[2] <- NA
+  out <- vMF(sample, km = inkm, method = "smfull")
+  cdabyppi:::expect_lte_v(abs(out$km - km), 3 * out$SE$km)
+})
+
+test_that("vMF() fitting works on dimension 5", {
+  set.seed(123)
+  p <- 5
+  k <- 3
+  m <- runif(p, min = -10, 10)
+  m <- m / sqrt(sum(m^2))
+  km <-  k * m
+  sample <- Directional::rvmf(1000, m, k)
+  #full method
+  out <- vMF(sample, method = "smfull", control = list(tol = 1E-10))
+  cdabyppi:::expect_lt_v(abs(out$km - km), 3 * out$SE$km)
+  #full with a fixed components
+  inkm <- km
+  inkm[2] <- NA
+  inkm[p] <- NA
+  out <- vMF(sample, km = inkm, method = "smfull")
+  cdabyppi:::expect_lte_v(abs(out$km - km), 3 * out$SE$km)
+  expect_equal(out$SE$km[!is.na(inkm)], rep(0, sum(!is.na(inkm))))
+
+  #Mardia method
+  out <- vMF(sample, method = "Mardia")
+  expect_equal(out$m , m, tolerance = 1E-1) #moment estimate part
+  cdabyppi:::expect_lt_v(abs(out$k - k), 3 * out$SE$k)
 })
