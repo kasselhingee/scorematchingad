dc2af9baeb041c17dbec46b016fc2e57ef394a40
Kassel Hingee <kassel.hingee@gmail.com>
2022-06-08 14:21:41 +1000

    bug fix to rhybrid test

--- tests/testthat/test-rhybrid.R
+++ tests/testthat/test-rhybrid.R
@@ -9,16 +9,16 @@ m <- sec2_3model(2)
 test_that("current PPI simulation method gives samples with similar empirical density estimates as the original simulation method", {
   # simulate using old method
   time_historic <- system.time(samp2 <- cdabyppi:::rhybrid_singly(n,m$p,m$beta0,m$ALs,m$bL,4))
-  H <- ks::Hpi(samp2$samp3[, -p])
-  kde_historic <- ks::kde(samp2$samp3[, -p], H)
+  H <- ks::Hpi(samp2$samp3[, -m$p])
+  kde_historic <- ks::kde(samp2$samp3[, -m$p], H)
   #simulate sample from PPI model
   time_current <- system.time(samp1 <- cdabyppi:::rhybrid(n,m$p,m$beta0,m$ALs,m$bL,4))
-  H <- ks::Hpi(samp1$samp3[, -p])
-  kde_current <- ks::kde(samp1$samp3[, -p], H)
+  H <- ks::Hpi(samp1$samp3[, -m$p])
+  kde_current <- ks::kde(samp1$samp3[, -m$p], H)
 
-  testresult <- ks::kde.test(samp2$samp3[, -p], samp1$samp3[, -p])
+  testresult <- ks::kde.test(samp2$samp3[, -m$p], samp1$samp3[, -m$p])
   expect_gte(testresult$pvalue, 0.01)
   time_ratio <- time_current/time_historic
-  expect_lt(time_ratio[["user.self"]], 0.02)
+  expect_lt(time_ratio[["user.self"]], 0.05)
 })
 
