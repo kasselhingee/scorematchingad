c596fa67f49b318d7254c49fa3d5fd0f2380c927
Kassel Hingee <kassel.hingee@gmail.com>
2022-06-20 19:58:50 +1000

    more to from symmetric matrices

--- R/ppi_toPPIparamvec.R
+++ R/ppi_toPPIparamvec.R
@@ -2,7 +2,7 @@
 #' @export
 toPPIparamvec <- function(ALs, bL, beta){
   stopifnot(isSymmetric.matrix(ALs))
-  theta <- c(diag(ALs), ALs[upper.tri(ALs)], bL, beta)
+  theta <- c(fromsmatrix(ALs), bL, beta)
   return(theta)
 }
 
@@ -19,10 +19,7 @@ fromPPIparamvec <- function(theta, p = NULL){
   calcp <- ppiltheta2p(length(theta))
   if(!is.null(p)){stopifnot(p == calcp)}
   p <- calcp
-  ALs <- matrix(NA, nrow = p - 1, ncol = p - 1)
-  diag(ALs) <- theta[1:(p - 1)]
-  ALs[upper.tri(ALs)] <- theta[p -1 + 1:((p-2) * (p-1)/2)]
-  ALs[lower.tri(ALs)] <- t(ALs)[lower.tri(ALs)]
+  ALs <- tosmatrix(theta[1:((p-1) + (p-1)*(p-2)/2)])
   bL <- theta[p - 1 + ((p-2) * (p-1)/2) + 1:(p-1)]
   beta <- theta[(p - 1 + ((p-2) * (p-1)/2) + (p-1) + 1):length(theta)]
   return(list(
@@ -50,37 +47,10 @@ ppiltheta2p <- function(ltheta){#ltheta is length of theta
   return(p)
 }
 
-#lAL_theta is the length of the AL component of the parameter vector
-combparam2uppertriorder_AL <- function(lAL_theta){
-  # l(AL_theta) = p-1 + (p-1) * (p-2)/2
-  # = p-1 + (p^2 -p -2p + 2)/2
-  # 2l = 2p-2 + p^2 - 3p + 2
-  # 0 = p^2  - p - 2l
-  # p = (1 pm sqrt(1 - 4*(-2l)))/2
-  #   = (1 pm sqrt(8l + 1)) /2 
-  #   = (1+sqrt(8l + 1) / 2
-  p <- (1+sqrt(8 * lAL_theta + 1)) / 2
-  sp <- p-1
-
-  #upper.tri ordering
-  AL <- matrix(NA, sp, sp)
-  diag(AL) <- 1:sp
-  AL[upper.tri(AL)] <- sp + (1:sum(upper.tri(AL)))
-  AL[lower.tri(AL)] <- t(AL)[lower.tri(AL)]
-
-  #comb ordering
-  inthetaorder <- c(diag(AL),
-                    AL[t(indexcombinations(p - 1)$ind)]) #each column is the own dimension, each row single element to extract
-
-  stopifnot(length(inthetaorder) == lAL_theta) #check that length produced is correct
-  stopifnot(all(inthetaorder[inthetaorder] == seq(1, length.out = lAL_theta))) #check that ordering index is correct
-  return(inthetaorder)
-}
-
 combparam2uppertriorder <- function(ltheta){
   p <- ppiltheta2p(ltheta)
   #order of AL part
-  inthetaorder <- combparam2uppertriorder_AL(p-1 + (p-1)*(p-2)/2)
+  inthetaorder <- combparam2uppertriorder_matrix(p-1 + (p-1)*(p-2)/2)
   # add rest of theta
   inthetaorder <- c(inthetaorder,
                     seq.int(max(inthetaorder) + 1, length.out = p - 1 + p))
@@ -88,6 +58,7 @@ combparam2uppertriorder <- function(ltheta){
   stopifnot(all(inthetaorder[inthetaorder] == seq(1, length.out = ltheta))) #check that ordering index is correct
   return(inthetaorder)
 }
+
 
 combparam2uppertriparam <- function(theta){
   inthetaorder <- combparam2uppertriorder(length(theta))
---
+++ R/symatrix2vec.R
@@ -0,0 +1,58 @@
+# tools for converting symmetric matrices to vectors and back
+
+tosmatrix <- function(theta){
+  # l = d + d * (d-1)/2
+  # 2l = 2d + d^2 - d
+  # 0 = d^2 + d - 2l
+  # d = (-1 pm sqrt(1-4*(-2l)))/2
+  #   = (-1 + sqrt(1+8l))/2
+  d <- (-1+sqrt(8 * length(theta) + 1)) / 2
+  mat <- matrix(NA, d, d)
+  diag(mat) <- theta[1:d]
+  mat[upper.tri(mat)] <- theta[(d + 1):length(theta)]
+  mat[lower.tri(mat)] <- t(mat)[lower.tri(mat)]
+  return(mat)
+}
+
+fromsmatrix <- function(mat){
+  stopifnot(isSymmetric.matrix(mat))
+  return(c(diag(mat), mat[upper.tri(mat)]))
+}
+
+# return the array of row/column indices, and the length
+# for the upper triangle
+indexcombinations <- function(d){
+  AL <- matrix(NA, d, d)
+  ind <- t(which(upper.tri(AL), arr.ind=TRUE))
+  qind=length(ind[1,])
+  return(list(ind = ind, qind = qind))
+}
+
+# for reordering a parameter vector created from a matrix using utils::combn
+#ltheta is the length of the parameter vector defining the matrix
+combparam2uppertriorder_matrix <- function(ltheta){
+  # l(theta) = p-1 + (p-1) * (p-2)/2
+  # = p-1 + (p^2 -p -2p + 2)/2
+  # 2l = 2p-2 + p^2 - 3p + 2
+  # 0 = p^2  - p - 2l
+  # p = (1 pm sqrt(1 - 4*(-2l)))/2
+  #   = (1 pm sqrt(8l + 1)) /2 
+  #   = (1+sqrt(8l + 1) / 2
+  p <- (1+sqrt(8 * ltheta + 1)) / 2
+  sp <- p-1
+
+  #upper.tri ordering
+  AL <- matrix(NA, sp, sp)
+  diag(AL) <- 1:sp
+  AL[upper.tri(AL)] <- sp + (1:sum(upper.tri(AL)))
+  AL[lower.tri(AL)] <- t(AL)[lower.tri(AL)]
+
+  #comb ordering
+  inthetaorder <- c(diag(AL),
+                    AL[t(indexcombinations(p - 1)$ind)]) #each column is the own dimension, each row single element to extract
+
+  stopifnot(length(inthetaorder) == ltheta) #check that length produced is correct
+  stopifnot(all(inthetaorder[inthetaorder] == seq(1, length.out = ltheta))) #check that ordering index is correct
+  return(inthetaorder)
+}
+
