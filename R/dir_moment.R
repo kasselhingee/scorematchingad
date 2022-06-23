333e36cedfa329cbae21ea8b0f263e86994ec64e
Kassel Hingee <kassel.hingee@gmail.com>
2022-06-23 13:12:38 +1000

    dirichmom with weights passes tests

--- R/dir_moment.R
+++ R/dir_moment.R
@@ -1,11 +1,14 @@
 #' @title Method of moments estimator for Dirichlet distribution
-#' @param X Compositional data (n by p matrix)
+#' @param Y Compositional data (n by p matrix)
+#' @param w A vector of weights to apply to the measurements in `Y`. The length of `w` must equal the number of rows of `Y`.
 #' @export
-dirichmom <- function(X) {
+dirichmom <- function(Y, w = rep(1, nrow(Y))) {
 # Method of Moments estimates of the Dirichlet Distribution
-temp <- dim(X); n <- temp[1]; m <- temp[2]
+temp <- dim(Y); n <- temp[1]; m <- temp[2]
 # X <- cbind(X,matrix(1-apply(X,1,sum)))
-mom <- apply(X,2,mean)*(mean(X[,1])-mean(X[,1]^2))/(mean(X[,1]^2) - ((mean(X[,1]))^2))
+cmeans <- apply(Y,2, weighted.mean, w = w)
+c1sqmean <- weighted.mean(Y[,1]^2, w = w)
+mom <- cmeans*(cmeans[1]-c1sqmean)/(c1sqmean - ((cmeans[1])^2))
 return(matrix(mom))
 }
 
---
+++ tests/testthat/test-weights-manyestimators.R
@@ -0,0 +1,36 @@
+#helper function: given a sample, simulate integer weights, then return weights and a version where each measurement is replicated by the weight size
+virtualweights <- function(Y, sizefactor = 1.5){
+  ind <- sample(1:nrow(Y), ceiling(sizefactor*nrow(Y)), replace = TRUE)
+  weights <- rep(0, nrow(Y))
+  weights[as.numeric(names(table(ind)))] <- table(ind)
+  newsample <- Y[ind, ]
+  return(list(
+    newY = newsample,
+    w = weights
+  ))
+}
+
+
+test_that("estimatorlog_weight matches for simulated weights", {
+  set.seed(1234)
+  m <- sec2_3model(1000, maxden = 4)
+  #simulate weights
+  set.seed(134)
+  vw <- virtualweights(m$sample)
+
+  est_sim <- estimatorlog_weight(vw$newY, betap = m$beta0[3], weightW = rep(1, nrow(vw$newY)))
+  est_direct <- estimatorlog_weight(m$sample, betap = m$beta0[3], weightW = vw$w)
+  expect_equal(est_direct$ppi, est_sim$ppi)
+})
+
+test_that("dirichmom matches for simulated weights", {
+  set.seed(1234)
+  m <- sec2_3model(1000, maxden = 4)
+  #simulate weights
+  set.seed(134)
+  vw <- virtualweights(m$sample)
+
+  est_sim <- dirichmom(vw$newY)
+  est_direct <- dirichmom(m$sample, w = vw$w)
+  expect_equal(est_direct, est_sim)
+})
