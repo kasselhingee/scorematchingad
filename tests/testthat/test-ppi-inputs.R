d89f2c60e2113dd7a7ce0dccd002e9e67f6924f2
Kassel Hingee <kassel.hingee@gmail.com>
2022-07-05 17:42:05 +1000

    bug fixes to test

--- tests/testthat/test-ppi-inputs.R
+++ tests/testthat/test-ppi-inputs.R
@@ -68,11 +68,11 @@ test_that("Inputs to ppi() are processed into the correct theta", {
 test_that("ppi with cppad method works easily on sec2_3model", {
   set.seed(1245)
   model <- sec2_3model(1000)
-  out <- ppi(model$sample, man = "sphere", weightname = "minsq", acut = 0.1)
+  out <- ppi(model$sample, man = "sphere", bdryweight = "minsq", acut = 0.1, method = "cppad")
   cdabyppi::expect_lt_v(abs(out$est$theta - model$theta) / out$SE$theta, 3)
 
   # try fixing betap
-  out <- ppi(model$sample, betap = -0.5, man = "sphere", hsqfun = "minsq", acut = 0.1)
+  out <- ppi(model$sample, betap = -0.5, man = "sphere", bdryweight = "minsq", acut = 0.1, method = "cppad")
   cdabyppi::expect_lte_v(abs(out$est$theta - model$theta), 3 * out$SE$theta)
   expect_equal(out$est$beta[model$p], -0.5)
   expect_equal(out$SE$beta[model$p], 0)
@@ -83,7 +83,7 @@ test_that("ppi with cppad method works easily on sec2_3model", {
   bL = rep(0, 2)
   prop <- rhybrid(100, 3, beta, AL, bL, 4)[[1]]
   theta <- toPPIparamvec(AL, bL, beta)
-  out <- ppi(prop, AL = "diag", betap = -0.5, man = "sphere", weightname = "minsq", acut = 0.1)
+  out <- ppi(prop, AL = "diag", betap = -0.5, man = "sphere", bdryweight = "minsq", acut = 0.1, method = "cppad")
   cdabyppi::expect_lte_v(abs(out$est$theta - theta), 3 * out$SE$theta)
   expect_equal(out$est$beta[model$p], -0.5)
   expect_equal(out$est$ALs[1, 2], 0)
@@ -94,7 +94,7 @@ test_that("ppi with cppad method works easily on sec2_3model", {
   bL = rep(0, 2)
   prop <- rhybrid(100, 3, beta, AL, bL, 4)[[1]]
   theta <- toPPIparamvec(AL, bL, beta)
-  out <- ppi(prop, AL = "diag", bL = 0, betap = -0.5, man = "Ralr", weightname = "ones")
+  out <- ppi(prop, AL = "diag", bL = 0, betap = -0.5, man = "Ralr", bdryweight = "ones", method = "cppad")
   cdabyppi::expect_lte_v(abs(out$est$theta - theta), 3 * out$SE$theta)
   expect_equal(out$est$beta[model$p], -0.5)
   expect_equal(out$est$ALs[1, 2], 0)
