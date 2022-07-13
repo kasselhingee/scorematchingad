03dae039c42ca6e6681f032bef54d08e6b613111
Kassel Hingee <kassel.hingee@gmail.com>
2022-07-13 12:11:24 +1000

    printgraph test added - and it is failing

---
+++ tests/testthat/test-pprintgraph.R
@@ -0,0 +1,8 @@
+
+test_that("Printing CppAD graph of tape works for dirichlet on simplex", {
+  beta <- c(-0.8, -0.5, 1)
+  pman <- pmanifold("simplex")
+  pppi <- ptapell(rep(0.1, length(beta)), beta, llname = "dirichlet", pman,
+                  fixedtheta = rep(FALSE, length(beta)), verbose = TRUE)
+  expect_error(pprintgraph(pppi))
+})
