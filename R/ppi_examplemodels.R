e018604918145d373109a42d49287291528da268
Kassel Hingee <kassel.hingee@gmail.com>
2022-07-23 16:43:10 -0400

    bug fixes, plus exporting beta as beta0 for temporary test compatibility

--- R/ppi_examplemodels.R
+++ R/ppi_examplemodels.R
@@ -1,54 +1,66 @@
-#' Make example models
+#' @title Build and simulate from example PPI models
 #' @param n Number of samples to generate
 #' @param maxden the log(C) maximum in simulating a PPI model
-#' @param betaadd A number to add to the beta0 parameters - to changing the skewness of the marginal distributions
+#' @param betaadd A number to add to the \eqn{\beta} parameter vector, which may be useful for experimenting with how 'Dirichlet' the model look.
 #' @return A list of the samples and model parameter information
 #' @examples
-#' sec2_3model(10, 4)
-#' sec2_3model_p4(1000, 8)
+#' sec2_3model(10)
+#' sec2_3model_p4(1000)
+#' @description 
+#' The model is the 3-component model from \insertCite{@Section 2.3, @scealy2022sc}{cdabyppi}.
+#' @references
+#' \insertAllCited{}
 #' @export
 sec2_3model <- function(n, maxden = 4, betaadd = 0){
-  #dimension
-  p=3
-
-  #parameters for the PPI model
-  muL=matrix(0,p-1,1)
-  muL[1:sum(p,-1)]=0.12
-  aa=matrix(1/500,p-1,1)
-  D=diag(as.vector(aa))
-  SigA=D
-  SigA[1,1]=SigA[1,1]*2
-  cor=0.5
-  SigA[1,2]=cor*sqrt(SigA[1,1]*SigA[2,2])
-  SigA[2,1]=SigA[1,2]
-  ALs=-0.5*solve(SigA)
-  bL=solve(SigA)%*%muL
-  beta0=matrix(-0.8,p,1)
-  beta0[p]=-0.5
-  beta0 = beta0 + betaadd
+  mats <- pars_sec2dot3model(3)
+  mats$beta = mats$beta + betaadd
 
   #simulate sample from PPI model
-  samp1=rppi(n,p,beta0,ALs,bL, maxden)
+  samp1=rppi(n,3,beta0 = mats$beta,ALs = mats$AL,bL = mats$bL, maxden)
 
   #simulated sample:
   samp3=samp1$samp3
 
-  return(list(
+  out <- c(list(
     sample = samp3,
-    p = p,
-    beta0 = beta0,
-    ALs = ALs,
-    bL = bL,
-    theta = toPPIparamvec(ALs, bL, beta0)
-  ))
+    p = 3,
+    theta = toPPIparamvec(mats$AL, mats$bL, mats$beta)
+    ),
+    mats,
+    list(beta0 = mats$beta))
+
+
+  return(out)
 }
 
 #' @describeIn sec2_3model
 #' @export
 sec2_3model_p4 <- function(n, maxden = 8){
-  #dimension
-  p=4
+  mats <- pars_sec2dot3model(4)
 
+  #simulate sample from PPI model
+  samp1=rppi(n,4,beta0 = mats$beta,ALs = mats$AL,bL = mats$bL, maxden)
+
+  #simulated sample:
+  samp3=samp1$samp3
+  
+  out <- c(list(
+    sample = samp3,
+    p = 3,
+    theta = toPPIparamvec(mats$AL, mats$bL, mats$beta)
+    ),
+    mats,
+    list(beta0 = mats$beta))
+
+
+  return(out)
+}
+
+
+# internal function for building Section 2.3-like model for any number of components
+# it pretty poor for anything but p = 3
+# returns the parameter matrices/vectors
+pars_sec2dot3model <- function(p){
   #parameters for the PPI model
   muL=matrix(0,p-1,1)
   muL[1:sum(p,-1)]=0.12
@@ -63,20 +75,9 @@ sec2_3model_p4 <- function(n, maxden = 8){
   bL=solve(SigA)%*%muL
   beta0=matrix(-0.8,p,1)
   beta0[p]=-0.5
-  theta <- c(diag(ALs), ALs[upper.tri(ALs)], bL, beta0)
-
-  #simulate sample from PPI model
-  samp1=rppi(n,p,beta0,ALs,bL,maxden)
-
-  #simulated sample:
-  samp3=samp1$samp3
-
   return(list(
-    sample = samp3,
-    p = p,
-    beta0 = beta0,
-    ALs = ALs,
+    AL = ALs,
     bL = bL,
-    theta = toPPIparamvec(ALs, bL, beta0)
+    beta = beta0
   ))
 }
