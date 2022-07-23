7df00d6e005d0794f5040bcaaf62bf2b54dd2cdd
Kassel Hingee <kassel.hingee@gmail.com>
2022-07-23 17:28:57 -0400

    renamed simplex bdry

--- R/simplex_bdryapprox.R
+++ R/simplex_bdryapprox.R
@@ -1,11 +1,11 @@
-#' @title Generate approximation centres for measurements on the boundary of the simplex
+# @title Generate approximation centres for measurements on the boundary of the simplex
 #' @param utable The compositional measurements
 #' @param shiftsize The distance to away from `u` and the simplex boundary to create the approximation centre.
 #' @return The approximation centres for the measurements
 #' @examples
 #' m <- ppi_egmodel(10)
 #' simplex_boundaryshift(m$sample, shiftsize = 1E-5)
-#' @export
+# @export
 simplex_boundaryshift <- function(utabl, shiftsize = 1E-4){
   p <- ncol(utabl)
   middleofsimplex <- rep(1/p, p)
@@ -21,14 +21,14 @@ approxcentre <- function(utabl, shiftsize = 1E-4){
   simplex_boundaryshift(utabl, shiftsize = shiftsize)
 }
 
-#' @title Return approximation centres for measurements near the simplex boundary.
+# @title Return approximation centres for measurements near the simplex boundary.
 #' @param u Measurement
 #' @param bdrythreshold Measurments closer than `bdrythreshold` to the edge of the simplex will be considered boundary measurements.
 #' @param shiftsize Boundary measurements are shifted `shiftsize` towards the simplex centre
 #' @examples
 #' u <- matrix(runif(5), nrow = 1)
 #' u <- u / sqrt(sum(u^2))
-#' @export
+# @export
 simplex_isboundary <- function(utabl, bdrythreshold = 1E-15){
   isbdrypt <- apply(utabl, MARGIN = 1, min) < bdrythreshold
   return(isbdrypt)
