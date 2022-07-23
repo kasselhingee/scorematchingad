8ac8d0b2d2ef53c542ce23cf59606414e873a0a4
Kassel Hingee <kassel.hingee@gmail.com>
2022-07-23 13:47:49 -0400

    deleted calculation of the score matching objective values for dirichlet distribution

--- tests/testthat/test-weights-manyestimators.R
+++ tests/testthat/test-weights-manyestimators.R
@@ -1,15 +1,3 @@
-#helper function: given a sample, simulate integer weights, then return weights and a version where each measurement is replicated by the weight size
-virtualweights <- function(Y, sizefactor = 1.5){
-  ind <- sample(1:nrow(Y), ceiling(sizefactor*nrow(Y)), replace = TRUE)
-  weights <- rep(0, nrow(Y))
-  weights[as.numeric(names(table(ind)))] <- table(ind)
-  newsample <- Y[ind, ]
-  return(list(
-    newY = newsample,
-    w = weights
-  ))
-}
-
 set.seed(1234)
 m <- sec2_3model(1000, maxden = 4)
 #simulate weights
@@ -80,8 +68,8 @@ test_that("dir_sqrt_minimah matches for simulated weights", {
   expect_equal(est_direct, est_sim)
 })
 
-test_that("score2_dir matches for simulated weights", {
-  est_sim <- score2_dir(vw$newY, acut = acut, beta = m$beta0)
-  est_direct <- score2_dir(m$sample, acut = acut, beta = m$beta0, w = vw$w)
+test_that("dir_sqrt_prodh matches for simulated weights", {
+  est_sim <- dir_sqrt_prodh(vw$newY, acut = acut)
+  est_direct <- dir_sqrt_prodh(m$sample, acut = acut, w = vw$w)
   expect_equal(est_direct, est_sim)
 })
