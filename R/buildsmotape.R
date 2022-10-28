3d71d269753bb2f684589a965a63a888fa9ce115
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-10-28 16:02:25 +1100

    help for buildsmotape more populated

--- R/buildsmotape.R
+++ R/buildsmotape.R
@@ -1,12 +1,32 @@
 # @title Internal function for building Score-Matching Objective Tapes
-#' @examples
 #' @param utape A measurment to use for taping
 #' @param intheta A vector of parameters. NA values will be estimated, non-NA values will be fixed.
 #' @param thetatape_creator A function that generates tape values for theta. Must take a single argument, `n` the number for values to generate
+#' @param manifoldname Manifold with tranformation name ('Ralr', 'simplex'...)
+#' @param llname Name of the log-likelihood function
+#' @param utape An example observation (a single vector) to use for taping. The results shouldn't depend on `utape` so long as `utape` is in the manifold and the other functions are properly implemented.
+#' @param usertheta A vector of parameter elements for the likelihood function. `NA` elements are to be fitted. Other elements are fixed at the provided value.
+#' @param weightname The name of the divergence weight function ('ones' for manifolds without boundary).
+#' @param acut The threshold `c` in the divergence weight function.
+#' @param thetatape_creator A function that accepts an integer `n`, and returns a vector of `n` length. The function is used to fill in the `NA` elements of `usertheta` when building the tapes.
+#' @param verbose If `TRUE` more details are printed when taping.
+#' @description
+#' Generates `CppAD` tapes for a model.
+#' @details
+#' When using, `CppAD` one first creates *tapes* of functions. These tapes can then be used for evaluating the function and its derivatives, and generating further tapes through argument swapping, differentiation and composition.
+#' The taping relies on specifying typical argument values for the functions, so the programming is simplest when the function is defined without conditions.
+#' Tapes can have both *independent* variables and *dynamic* variables.
+#' The differentiation with `CppAD` occurs with respect to the independent variables.
+#' Tapes of tapes are possible, including tapes that swap the independent and dynamic variables - this is how this package differentiates with respect to a dynamic variables.
+#'
+#' To build a tape for the score matching objective function, the package first tapes the map from a point \eqn{z} on the manifold to the value of the log-likelihood, where the independent variable is the \eqn{z}, and the dynamic variable is a vector of the *non*-fixed parameter values.
+#' This tape is then used to generate a tape for the score matching objective with the non-fixed parameter values as the independent variable.
+#' @return
+#' Returns a list of the log-likelihood tape (note that the *input* for this tape is a function on the manifold), the tape of the score matching objective (the *input* here is the non-fixed parameter values), and some information used to generate the tape.
 #' @examples
 #' p <- 3
-#' u <- Directional::rvmf(1, rep(1, p), 0) #uniform distribution on the sphere p-space
-#' ltheta <- p-1 + (p - 1) * p/2 + p
+#' u <- movMF::rmovMF(1, rep(1, p))
+#' ltheta <- p-1 + (p - 1) * p/2 + p #length of Fisher-Bingham parameter vector
 #' intheta <- rep(NA, length.out = ltheta)
 #' tapes <- buildsmotape("Snative", "FB",
 #'               u, intheta,
