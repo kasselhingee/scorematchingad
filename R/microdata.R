32ab71da729f26b6a5fae00f48ff97cb52951a38
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-10-31 15:39:24 +1100

    microdata help

--- R/microdata.R
+++ R/microdata.R
@@ -1,10 +1,12 @@
 #' Microbiome Data from Article Supplement
 #'
-#' This file was created from the file `microbiome_data.xlsx` in the supplementary information for the log-ratio paper.
-#' It two extra measurements than the `microbiome_data.xlsx` file in the supplementary information for JASA 2022 paper.
-#' These two extra measurements are outliers and have `IndividualID` of `2079` and `2280`.
+#' This file was created from the file `microbiome_data.xlsx` in the supplementary information of the log-ratio draft manuscript.
+#' It contains two more measurements than the `microbiome_data.xlsx` file in the supplementary information of \insertCite{scealy2022sc}{scorecompdir},
+#' which were deemed outliers by Scealy and Wood, and have `IndividualID` of `2079` and `2280`.
+#' 
+#' This data is currently private, pending permission from the original creators of the data.
 #'
-#' The file was created using the below
+#' The file was created `microbiome_data.xlsx` using the below code
 #'
 #' ```
 #' microdata <- readxl::read_excel("microbiome_data.xlsx")
@@ -13,5 +15,7 @@
 #' ```
 #'
 #' @format A `data.frame` with 94 rows and 31 columns.
-#' @source is there a source?
+#' @source To be confirmed.
+#' @references
+#' \insertAllCited{}
 "microdata"
