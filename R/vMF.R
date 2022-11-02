b37a4f02dce55e80a778173d1f981a40f05ebb38
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-11-02 15:59:44 +1100

    vMF help updated

--- R/vMF.R
+++ R/vMF.R
@@ -1,12 +1,30 @@
-#' @title Score matching estimate for the von-Mises Fisher distribution
-#' @details The parameter vector for vMF is the `K * m` where `k` is the concentration, `m` is the mean direction.
-#' @param sample Samples from the vMF distribution in cartesian Rd coordinates. Each row is a measurement.
-#' @param paramvec Optional for the `smfull` method. A vector of same length as the dimension, representing the elements of the concentraction * Fisher direction vector. It is `k * m` where `k` is the concentration, `m` is the mean direction.
-#' If supplied, the non-NA elements are fixed. It is easy to generate `paramvec` using [vMF_paramvec()].
-#' @param control Control parameters passed to `Rcgmin::Rcgmin()`.
-#' @param method Either `Mardia` for the hybrid score matching estimate from Mardia et al 2016
-#'  or `smfull` for a full score matching estimate.
-#' @param w An optional vector of weights for each measurement in `sample`
+#' @title Score matching estimator for the von-Mises Fisher distribution
+#' @description
+#' We recommend using [`movMF::movMF()`] over the function here, `vMF()`, for estimating the von Mises Fisher distribution.
+#' 
+#' \insertCite{@mardia2016sc} suggest score matching for the von Mises Fisher distribution to avoid the difficult-to-compute normalising constant of the von Mises Fisher distributions.
+#' \insertCite{@mardia2016sc} suggested both full score matching and a hybrid estimator, where by the mean direction is estimated via the maximum-likelihood approach and the concentration is estimated via score matching.
+#' However, \insertCite{@mardia2016sc} found, consistent with some of our preliminary experiments, that these score matching estimators were not as a efficient as the maximum likelihood estimators (e.g. [`movMF::movMF()`]).
+#' @section von Mises Fisher Model: 
+#' The von Mises Fisher density is proportional to
+#' \deqn{\exp(\kappa \mu^T z),}
+#' where \eqn{z} is on a unit sphere,
+#' \eqn{kappa} is termed the *concentration*,
+#' and \eqn{\mu} is the *mean direction vector*.
+#' The effect of the \eqn{\mu} and \eqn{\kappa} can be decoupled in a sense \insertCite{@mardia2000??}{scorecompdir}, allowing for estimating \eqn{\mu} and \eqn{\kappa} separately.
+#'
+#' The method "smfull" uses score matching to estimate the vector \eqn{\kappa \mu}.
+#' The method "Mardia" uses [`vMF_stdY()`] and [`vMF_kappa()`] to estimate the parameters of the von Mises Fisher distribution.
+
+#' @details
+#' The parameter vector for vMF is the `K * m` where `k` is the concentration, `m` is the mean direction.
+#' @param Y A matrix of multivariate observations in Cartesian coordinates. Each row is a measurement.
+#' @param paramvec `smfull` method only: Optional. A vector of same length as the dimension, representing the elements of the vector \eqn{\kappa \mu}. 
+#' If supplied, the non-NA elements are fixed. It is easy to generate `paramvec` using [`vMF_paramvec()`].
+#' @param control Control parameters passed to [`Rcgmin::Rcgmin()`].
+#' @param method Either "Mardia" for the hybrid score matching estimator from \insertCite{@mardia2016sc}{scorecompdir}.
+#'  or "smfull" for the full score matching estimator.
+#' @param w An optional vector of weights for each measurement in `Y`
 #' @examples
 #' set.seed(12342)
 #' Y <- movMF::rmovMF(1000, 100 * c(1, 1) / sqrt(2))
@@ -15,9 +33,9 @@
 #' vMF(Y, method = "Mardia")
 #' @return
 #' A list of `est`, `SE` and `info`.
-#' `est` contains the estimates in vector form, `paramvec`, and with user friendly names `k` and `m`.
-#' `SE` contains estimates of the standard errors if computed by the estimating method. Standard error estimates of `k` and `m` are not computed.
-#' `info` contains a variety of information about the model fitting procedure.
+#'  * `est` contains the estimates in vector form, `paramvec`, and with user friendly names `k` and `m`.
+#'  * `SE` contains estimates of the standard errors if computed.
+#'  * `info` contains a variety of information about the model fitting procedure and results.
 #' @export
 vMF <- function(Y, paramvec = NULL, method = "smfull", control = default_Rcgmin(), w = rep(1, nrow(Y)), paramvec_start = NULL){
   firstfit <- NULL
