f0870d1d3cca0b95ffa9fe65b1d3df3e2b142e48
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-11-02 12:56:47 +1100

    got -package.Rd documentation working in scorecompdir-package.R

--- R/scorecompdir-package.R
+++ R/scorecompdir-package.R
@@ -1,5 +1,7 @@
+#' @import Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib scorecompdir
 #' @details
 #' For some models the density approaches infinity at some locations, creating difficulties for Windham's weights \insertCite{@scealy2022draft}{scorecompdir}.
-#' @keywords internal
 "_PACKAGE"
 
