f0f79d650ee12baf0897578ab994d8226dc72196
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-11-02 12:37:15 +1100

    updates to ppi_cW and scorecompdir-package help created

--- R/ppi_cW.R
+++ R/ppi_cW.R
@@ -1,10 +1,20 @@
-#' @title Generate default vector of Windham exponents for the PPI model
-#' @description For the PPI model, a good default is for Windham exponents for every element of the \eqn{A_L}{A_L} matrix that corresponds to a component with concentrations near zero.
-#' @param cW The value of the Windham exponent, the same for each element corresponding to the component.
-#' @param ... Values of `TRUE` or `FALSE` in the same order of the components specifying if each component has a concentration near zero.
+#' @title Quickly generate a vector of Windham exponents for the PPI model
+#' @description These functions help to quickly generate a set of Windham exponents for use in [`ppi_robust()`] or [`Windham()`].
+#' Elements of \eqn{A_L} and \eqn{b_L} corresponding to components with strong concentrations have non-zero constant tuning exponent, and all other elements have a tuning constant of zero.
+#' All elements of \eqn{\beta} have a tuning exponent of zero.
+#' @param cW The value of the non-zero Windham tuning exponents.
+#' @param ... Values of `TRUE` or `FALSE` in the same order of the components specifying that a component has a concentration near zero.
 #' @return A vector of the same length as the parameter vector of the PPI model. Elements of \eqn{A_L}{A_L} will have a value of `cW` if both their row and column component has probability mass concentrated near zero. Similarly, elements of \eqn{b_L} will have a value of `cW` if their row corresponds to a component that has a concentration near zero. All other elements are zero.
 #' @details
-#' The final component is not included in \eqn{A_L} or \eqn{b_L} and so this function assumes that all parameters for the final component have a Windham exponent of `0`.
+#' The Windham robustifying method involes weighting observations by a function of the proposed model density \insertCite{@windham1995ro}{scorecompdir}.
+#' For discretised compositional observations (e.g. microbiome data),
+#' observed components with a value of zero are common,
+#' creating weights of infinity for models with concentrations at zero \insertCite{@scealy2022draft}{scorecompdir}.
+#' \insertCite{@scealy2022draft}{scorecompdir} avoided these infinite-valued weights by setting some parameters of the PPI model to zero when calculating the weights.
+#' \insertCite{@scealy2022draft}{scorecompdir} chose tuning exponents of zero for:
+#'  * elements of \eqn{\beta} corresponding to concentrations near zero, which avoids infinte weights
+#'  * any parameter elements corresponding to components that do not have a strong concentration, because outliers are not possible on the simplex if there isn't a strong concentration.
+#' The remaining tuning exponents were set to a constant.
 #' @export
 ppi_cW <- function(cW, ...){
   bools <- unlist(list(...))
@@ -22,7 +32,8 @@ ppi_cW <- function(cW, ...){
 }
 
 #' @param Y A matrix of observations
-#' @describeIn ppi_cW Automatically detects concentrations near zero by fitting a PPI distribution with \eqn{A_L=0} and \eqn{b_L=0} (i.e. a Dirichlet distribution) with the square root transformation and minima-based divergence weight function.
+#' @rdname ppi_cW
+#' @details The function `ppi_cW_auto()` automatically detects concentrations near zero by fitting a PPI distribution with \eqn{A_L=0} and \eqn{b_L=0} (i.e. a Dirichlet distribution) with the square root transformation and minima-based divergence weight function.
 #' @examples
 #' Y <- ppi_egmodel(100)$sample
 #' ppi_cW_auto(0.01, Y)
---
+++ R/scorecompdir-package.R
@@ -0,0 +1,5 @@
+#' @details
+#' For some models the density approaches infinity at some locations, creating difficulties for Windham's weights \insertCite{@scealy2022draft}{scorecompdir}.
+#' @keywords internal
+"_PACKAGE"
+
