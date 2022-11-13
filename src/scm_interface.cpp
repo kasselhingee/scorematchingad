4c50d7f233acea09013a32c7e1ff9365ca395132
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-11-13 13:07:58 +1100

    wrap() implementation seems to have worked, trying as()

--- src/scm_interface.cpp
+++ src/scm_interface.cpp
@@ -272,30 +272,16 @@ XPtr< CppAD::ADFun<double> > swapDynamic(XPtr< CppAD::ADFun<double> > pfun, svec
 //' @return The Jacobian of pfun
 // @export
 // [[Rcpp::export]]
-vecd pJacobian(XPtr< CppAD::ADFun<double> > pfun, svecd value, svecd theta){
-  //convert input to an Eigen vectors
-  vecd value_e(value.size());
-  for (long int i=0; i<value.size(); i++){
-    value_e[i] = value[i];
-  }
-  vecd theta_e(theta.size());
-  for (long int i=0; i<theta.size(); i++){
-    theta_e[i] = theta[i];
-  }
+vecd pJacobian(XPtr< CppAD::ADFun<double> > pfun, vecd value, vecd theta){
 
   //check inputs and tape match
-  if (pfun->Domain() != value_e.size()){stop("Size of input vector %i does not match domain size %i of taped function.", value_e.size(), pfun->Domain());}
-  if (pfun->size_dyn_ind() != theta_e.size()){stop("Size of parameter vector %i does not match parameter size %i of the taped function.", theta_e.size(), pfun->size_dyn_ind());}
+  if (pfun->Domain() != value.size()){stop("Size of input vector %i does not match domain size %i of taped function.", value.size(), pfun->Domain());}
+  if (pfun->size_dyn_ind() != theta.size()){stop("Size of parameter vector %i does not match parameter size %i of the taped function.", theta.size(), pfun->size_dyn_ind());}
 
-  vecd grad(value_e.size());
-  svecd out(value_e.size());
-  pfun->new_dynamic(theta_e);
-  grad = pfun->Jacobian(value_e);  //treat the XPtr as a regular pointer
+  vecd grad(value.size());
+  pfun->new_dynamic(theta);
+  grad = pfun->Jacobian(value);  //treat the XPtr as a regular pointer
 
-  //convert to std::vector
-  for (long int i = 0; i<grad.size(); i++){
-    out[i] = grad[i];
-  }
   return(grad);
 }
 
