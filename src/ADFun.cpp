69e1dc760c1cdab52375fd3fe28549cd3286d88d
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-11-14 17:16:13 +1100

    Revert "package is installing with ADFun_double module inside package, but not used"
    This reverts commit 4db925e6450008f5f0144e739e9b7939ff4f4664.

--- src/ADFun.cpp
+++ src/ADFun.cpp
@@ -6,27 +6,23 @@
 // see cppad/core/ad_fun.hpp for the definition of ADFun in CppAD
 
 
-#include "scorecompdir_types.h"
+#include <RcppEigen.h>
 //#include "scm_interface.cpp"
 # include <cppad/cppad.hpp>
-# include <Rcpp.h>
 using namespace Rcpp;
 
-// class ADFun_double defined as a special version of the templated ADFun
-// following how RcppAnnoy has done it
-RCPP_EXPOSED_CLASS_NODECL(ADFun_double)
-RCPP_MODULE(ADFun_double) {
 
-  class_<ADFun_double>("ADFun")
+RCPP_MODULE(ADFun_module) {
+
+  class_<CppAD::ADFun<double> >("ADFun")
 
   .constructor() //the default constructor
 
   //would love the copy constructor too, but want to stop R from actually copying
 
-  //.method("Domain", &CppAD::ADFun<double>::Domain, "The dimension of the domain (i.e. the length of the vector that this ADFun evaluates")
+  .method("Domain", &CppAD::ADFun<double>::Domain, "The dimension of the domain (i.e. the length of the vector that this ADFun evaluates")
 
   ; //in the Rcpp vignette on modules there is a single semi-colon at the end of the definitions
 }
-
 
 # endif
