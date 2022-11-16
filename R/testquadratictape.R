52fd9eaca06368a174167a8e664d48c0c98844fc
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-11-17 09:28:02 +1100

    quadratic tape test drafted

--- R/testquadratictape.R
+++ R/testquadratictape.R
@@ -2,12 +2,19 @@
 #' @description
 #' Uses [`pParameter()`] and derivatives [`pTapeJacobian()`], [`pJacobian()`] to test whether
 #' the tape is quadratic.
-#' Also returns some information about the Hessian.
+#' @param tape An `Rcpp::XPtr` to a CppAD tape.
 #' @param xmat If passed, the third-order derivatives at values of the rows of `xmat` are tested.
 #' @param dynparammat The dynamic parameters for the tape. If passed, the rows of `dynparammat` are passed to the tape as `dynparam`.
 #' @details Uses the `xtape` and `dyntape` attributes of `tape` to create new tapes.
 #' A tape of Hessian is obtained by applying [`pTapeJacobian()`] twice. Using [`pTapeHessian()`] directly did not show constant parameters via [`pParameter()`] in tests.
+#'
+#' Two tests are conducted on the tape of the Hessian.
 #' [`pParameter()`] is evaluated with dynamic parameters given by the `dyntape` attribute of `tape`.
+#' For a function of quadratic form, [`pParameter()`] should return a vector of `TRUE` values.
+#' The other test evaluates the Jacobian at each row of `xmat` and `dynparammat` (if `xmat` and `dynparammat` are non-NULL). The result should be zero when `tape` represents a function of quadratic form.
+#' If the results of the tests differ the returned value is `FALSE` and a message is printed indicating which test failed.
+#' @return `TRUE` or `FALSE`
+#' @export
 testquadratictape <- function(tape, xmat = NULL, dynparammat = NULL){
   tapeJ <- pTapeJacobian(tape, attr(tape, "xtape"), attr(tape, "dyntape"))
   tapeH <- pTapeJacobian(tapeJ, attr(tape, "xtape"), attr(tape, "dyntape"))
@@ -16,30 +23,28 @@ testquadratictape <- function(tape, xmat = NULL, dynparammat = NULL){
   isparameter <- pParameter(tapeH, attr(tape, "dyntape"))
   result_pParameter <- all(isparameter)
 
+  if (is.null(xmat) && is.null(dynparammat)){return(result_pParameter)}
+
   #try Jacobian
-  result_thirdderiv <- NA
-  if (!is.null(xmat) || !is.null(dynparammat)){
-    stopifnot(nrow(xmat) == nrow(dynparammat))
-    thirdderivs <- lapply(1:nrow(xmat), function(i){
-      pJacobian(tapeH, xmat[i, ], dynparammat[i, ])
-    })
-    isallzero <- unlist(lapply(thirdderivs, function(vec){all(vec == 0)}))
-    result_thirdderiv <- all(isallzero)
-  }
+  stopifnot(isTRUE(nrow(xmat) == nrow(dynparammat)))
+  thirdderivs <- lapply(1:nrow(xmat), function(i){
+    pJacobian(tapeH, xmat[i, ], dynparammat[i, ])
+  })
+  isallzero <- unlist(lapply(thirdderivs, function(vec){all(vec == 0)}))
+  result_thirdderiv <- all(isallzero)
 
-  #properties of Hessian. If Hessian is constant it should matter the values used
-  propsHessian <- list()
-  if (isTRUE(result_pParameter) | isTRUE(result_thirdderiv)){
-    if (is.null(xmat)){
-      hess <- pForward0(tapeH, attr(tape, "xtape"), attr(tape, "dyntape"))
-    } else {
-      hess <- pForward0(tapeH, xmat[1, ], dynparammat[1, ])
+  finalresult <- result_thirdderiv && result_pParameter
+  if (result_thirdderiv != result_pParameter){
+    if (!result_thirdderiv){
+      message(sprintf("The Jacobian of the Hessian was non-zero for row %s of xmat and dynparammat",
+                      paste(which(!isallzero, collapse = ", "))))
     }
-    hessmat <- matrix(hess, byrow = TRUE, ncol = sqrt(length(hess)))
-    propsHessian$symmetric <- isSymmetric(hessmat)
-    propsHessian$determinant <- determinant(hessmat, logarithm = FALSE)
-    propsHessian$evalues <- eigen(hessmat)$values
-    propsHessian$posdefinite <- all(propsHessian$evalues > 0) #a symmetric matrix is positive definite iff all its eigen values are positive
-  } 
-
+    if (!result_pParameter){
+      message(sprintf("The Hessian was non-constant according to pParameter() for elements %s.",
+                      paste(which(!isparameter, collapse = ", "))))
+    }
+  }
+  return(finalresult)
 }
+
+
