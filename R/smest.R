0285efc73228be2766a7d6cf8f93a9bf5d9d6d40
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-11-28 17:37:41 +1100

    removed smest() from packaged

--- R/smest.R
+++ R/smest.R
@@ -1,48 +1,3 @@
-#' @noRd
-#' @title Find the optimal parameter for the score matching objective
-#' @description Using a numerical optimiser and `smobj()` and `smobjgrad()`, find the parameter set with minimal `smobj` value.
-#' This function lightly wraps `Rcgmin::Rcgmin()` with some results checking.
-#' @param smofun A tape of the score matching objective calculation
-#' @param theta The starting parameter set
-#' @param utabl A matrix of observations, each row being an observation.
-#' @param uboundary Optional. A matrix of observations close to or on the boundary.
-#' The contribution of the score matching objective (and corresponding derivatives) of these measurements
-#' will be approximated using Taylor approximation from the corresponding `boundaryapprox` matrix.
-#' @param boundaryapprox Optional. A matrix of locations in the manifold associated with each measurement in `boundary`.
-#' For best results these locations should be further from the manifold boundary and close to their corresponding measurements.
-#' Taylor approximation around the rows of `boundaryapprox` will be used to approximate the score matching objective for these measurements.
-#' @param control Optional argument passed to `Rcgmin::Rcgmin()`.
-#' @return The output from `Rcgmin::Rcgmin()`, the squared size of the gradient at the estimate, and the standard error estimates by `cppadSE()`.
-# @export
-cppadest <- function(smofun, theta, utabl, control = default_Rcgmin(), uboundary = NULL, boundaryapprox = NULL, approxorder = NULL, w = NULL, wboundary = NULL){
-  if (!(is.null(uboundary) && is.null(boundaryapprox))){
-    stopifnot((!is.null(uboundary)) && (!is.null(boundaryapprox))) #both need to be supplied
-    stopifnot(nrow(uboundary) == nrow(boundaryapprox))
-    stopifnot(ncol(uboundary) == ncol(boundaryapprox))
-    stopifnot(ncol(uboundary) == ncol(utabl))
-  }
-  
-  # convert old-style boundary specification into new style
-  boundaryapprox <- rbind(NA * utabl, boundaryapprox)
-  utabl <- rbind(utabl, uboundary)
-  w <- c(w, wboundary)
-  if (is.null(w)){w <- rep(1, nrow(utabl))}
-
-  # fake xtape and dyntape attributes
-  if(!isTRUE(nrow(utabl) > 0)){
-    warning("Guessing an interior point for taping")
-    p <- ncol(utabl)
-    eginteriorpt <- rep(1/p, p)
-  } else {
-    eginteriorpt <- utabl[1, ]
-  }
-  attr(smofun, "xtape") <- theta
-  attr(smofun, "dyntape") <- eginteriorpt
-  out <- cppad_search(smotape = smofun, theta = theta, Y = utabl, Yapproxcentres = boundaryapprox,
-                      w = w, approxorder = approxorder, control = control)
-  return(out)
-}
-
 # same parameters as cppad_closed
 #' @title Iterative Score Matching Estimator Using Conjugate-Gradient Descent
 #' @description 
@@ -55,6 +10,7 @@ cppadest <- function(smofun, theta, utabl, control = default_Rcgmin(), uboundary
 #' The `smotape`'s independent variables are assumed to be the model parameters to fit
 #' and the `smotape`'s dynamic parameter is a (multivariate) measurement.
 #' @param Y A matrix of multivariate observations. Each row is an observation.
+#' @param theta The starting parameter set
 #' @param w Weights for each observation.
 #' @param approxorder The order of Taylor approximation to use.
 #' @param control Control parameters passed to [`Rcgmin::Rcgmin()`]
@@ -125,5 +81,9 @@ cppad_search <- function(smotape, theta, Y, Yapproxcentres = NA * Y, w = rep(1, 
   out$par <- NULL
   return(out)
 }
-
+#' @return A list of
+#' + `est` The estimate
+#' + `sqgradsize` The squared size of the gradient at the estimate
+#' + `SE` The standard error of the estimates when `w` is constant. If `w` changes between observations then this slot contains the string "Not calculated."
+#' + additional output from [`Rcgmin::Rcgmin()`]
 
