7d1c48403b19237e9ab4a9aa679d18d4ae428c14
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-12-13 11:25:37 +1100

    spelling correction

--- tests/testthat/test-tape_eval.R
+++ tests/testthat/test-tape_eval.R
@@ -1,4 +1,4 @@
-test_that("PPI ALR hardcoded estimate has low smo and smgrad values and contant Hessian and offset", {
+test_that("PPI ALR hardcoded estimate has low smo and smgrad values and constant Hessian and offset", {
   mnongamma <- ppi_egmodel(1)
   theta <- ppi_paramvec(beta = c(-0.95, -0.9, 0.5), AL = mnongamma$AL, bL = 0)
   set.seed(1234)
