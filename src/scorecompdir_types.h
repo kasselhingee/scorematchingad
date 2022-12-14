54cf507e4c502704bda017ba88c0f2e9ab128a47
Kassel Hingee <kassel.hingee@anu.edu.au>
2022-12-14 11:15:02 +1100

    cleaning up scorecompdir_types - removing the 'using Rcpp' call as stackoverflow suggests it is bad

--- src/scorecompdir_types.h
+++ src/scorecompdir_types.h
@@ -8,9 +8,6 @@
 # include <cppad/cppad.hpp> // the CppAD package
 # include <cppad/utility/index_sort.hpp> //for index sorting - for Rivest model
 
-
-using namespace Rcpp;
-
 typedef std::vector<double> svecd;
 typedef Eigen::Matrix<double, Eigen::Dynamic, 1> vecd; //a vector of double values
 typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> matd;//a matrix of double
@@ -19,15 +16,6 @@ typedef Eigen::Matrix<a1type, Eigen::Dynamic, 1> veca1; //a vector of a1type val
 typedef Eigen::Matrix<a1type, Eigen::Dynamic, Eigen::Dynamic> mata1;//a matrix of a1types
 typedef CppAD::AD<a1type> a2type;  // for second (inner) level of taping
 typedef Eigen::Matrix<a2type, Eigen::Dynamic, 1> veca2;
-
-// template <typename T>
-// struct manifold {
-//   Eigen::Matrix<T, Eigen::Dynamic, 1> (*toM)(const Eigen::Matrix<T, Eigen::Dynamic, 1> &); //map from simplex to manifold
-//   Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> (*Pmatfun)(const Eigen::Matrix<T, Eigen::Dynamic, 1> &); //projection matrix for manifold
-//   Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> (*dPmatfun)(const Eigen::Matrix<T, Eigen::Dynamic, 1> &, const int &); //elementwise derivative of projection matrix for manifold
-//   Eigen::Matrix<T, Eigen::Dynamic, 1> (*fromM)(const Eigen::Matrix<T, Eigen::Dynamic, 1> &); //transformation from manifold to simplex
-//   T (*logdetJfromM)(const Eigen::Matrix<T, Eigen::Dynamic, 1> &); //determinant of Jacobian of the tranformation - for correcting the likelihood function as it is a density
-// };
 
 template <typename T>
 struct manifold { //exactly like a class, but with default public members https://stackoverflow.com/questions/24196885/can-c-struct-have-member-functions
