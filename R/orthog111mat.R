e1014333e004e80fb503fa4af66b3453ca98bb50
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-01-04 16:56:33 +1100

    orthog111mat uses rotationmatrix, and I think I noticed a bug of solve(rmat1) rather than solve(rmat2)

--- R/orthog111mat.R
+++ R/orthog111mat.R
@@ -1,7 +1,6 @@
 # A helper function for creating test Astar matrices
 orthogmatwith111vec <- function(){
   Q <- mclust::randomOrthogonalMatrix(3, 3)
-  rotmat1 <- vec2northpole(Q[,3])
-  rotmat2 <- vec2northpole(rep(1, 3) / sqrt(3))
-  return(rotmat2 %*% solve(rotmat1) %*% Q)
+  Rmat <- rotationmatrix(rep(1, 3), Q[, 3])
+  return(Rmat %*% Q)
 }
