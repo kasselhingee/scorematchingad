feea7ace9e9f09fa841e56127888d09b4d3ea829
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-01-04 17:10:31 +1100

    all tests related to rotation matrix are passing

--- tests/testthat/test-rotatiomatrix.R
+++ tests/testthat/test-rotatiomatrix.R
@@ -1,34 +1,15 @@
 test_that("Rotation matrix generation on high dimensions", {
   u <- c(1, 2, 3, 4, 5, 6)
-  Rmat <- vec2northpole(u)
-  newu <- Rmat %*% u
-  expect_equal(newu[1], sqrt(sum(u^2)))
-  expect_equal(newu[-1], rep(0, length(u) - 1))
-
-  u2 <- c(10, 100, 3, -10, 7, 0)
-  expect_equal(sqrt(sum((Rmat %*% u2)^2)), sqrt(sum(u2^2)))
-
-  u3 <- c(2, 0, 0, 0, 0, 0)
-  expect_equal(sqrt(sum((Rmat %*% u3)^2)), 2)
-
-  u4 <- c(6, 5, 4, 3, 2, 1)
-  expect_equal(sqrt(sum((Rmat %*% u4)^2)), sqrt(sum(u4^2)))
+  b <- c(3,4,5,1,2,6)
+  Rmat <- rotationmatrix(u, b)
+  expect_equal(u, Rmat %*% b, ignore_attr = TRUE)
 })
 
-test_that("Rotation matrix with inverse gets to any direction", {
-  u <- c(1, 2, 3, 4, 5, 6)
-  uend <- c(3, 4, 1, 2, 5, 6)
-  Rmat1 <- vec2northpole(u)
-  Rmat2 <- vec2northpole(uend)
-  Rmat <- solve(Rmat2) %*% Rmat1
-
-  expect_equal(uend, Rmat %*% u, ignore_attr = TRUE) #because size of u == size of uend
-})
 
 test_that("Handedness of coord system is preserved", {
   vector.cross <- function(a, b) { #code from https://stackoverflow.com/questions/15162741/what-is-rs-crossproduct-function
     if(length(a)!=3 || length(b)!=3){
-      stop("Cross product is only defined for 3D vectors.");
+      stop("This cross product function is only defined for 3D vectors.");
     }
     i1 <- c(2,3,1)
     i2 <- c(3,1,2)
@@ -45,17 +26,14 @@ test_that("Handedness of coord system is preserved", {
   #so a transformation R of e1, e2, e3 that preserves sizes, angles and handedness should be s.t.
   # Re1 x Re2 = Re3
   u <- c(1, 2, 3)
-  Rmat <- vec2northpole(u)
+  Rmat <- rotationmatrix(e1, u)
 
   expect_equal(vector.cross(Rmat %*% e1, Rmat %*% e2), Rmat %*% e3,
                ignore_attr = TRUE)
 
-  RmatB <- Directional::rotation(u/sqrt(sum(u^2)), c(1, 0, 0))
-
-  RmatB %*% e1
-  RmatB %*% e2
-  RmatB %*% e3
-  expect_equal(vector.cross(RmatB %*% e1, RmatB %*% e2), RmatB %*% e3,
+  # also rotating e2 to e1 doesn't just switch the order of the dimensions
+  Rmat <- rotationmatrix(e1, e2)
+  expect_equal(vector.cross(e3, Rmat %*% e2), e2,
                ignore_attr = TRUE)
 })
 
@@ -70,7 +48,7 @@ test_that("rotation is the simplest for e1, e2, e3", {
   e1 <- c(1, 0, 0)
   e2 <- c(0, 1, 0)
   e3 <- c(0, 0, 1)
-  Rmat <- vec2northpole(e3)
+  Rmat <- rotationmatrix(e1, e3)
   # Rmat <- Directional::rotation(e3, e1)
   expect_equal(Rmat %*% e3, e1, ignore_attr = TRUE)
   expect_equal(Rmat %*% e2, e2, ignore_attr = TRUE)
