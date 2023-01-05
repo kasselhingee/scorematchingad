c37e34808343a24eca540ec36adbdcfc7b11962d
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-01-05 17:00:42 +1100

    first draft attempt of more analytical determinat of clr transformation

--- src/mycpp/manifold_Hclr.hpp
+++ src/mycpp/manifold_Hclr.hpp
@@ -2,6 +2,7 @@
 #define mycpp_manifold_Hclr
 // code for various tools for the additive log ratio transform
 #include <RcppEigen.h>
+#include <cppad/cppad.hpp> //for CppAD::log
 namespace mantran {//names space for manifold-transformation pair (triplets}
 template <typename Type>
 struct Hclr : public manifold<Type> {
@@ -36,6 +37,18 @@ struct Hclr : public manifold<Type> {
 
 
      return(out2);
+  }
+  
+  Type logdetJfromM(const Eigen::Matrix<Type, Eigen::Dynamic, 1> &z) override {
+    Eigen::Matrix<Type, Eigen::Dynamic, 1> u(z.size());
+    u = fromM(z);
+    Eigen::Matrix<Type, Eigen::Dynamic, 1> unotp(u.size() - 1);
+    unotp = u.head(u.size() - 1);
+    Eigen::Matrix<Type, Eigen::Dynamic, Eigen::Dynamic> jac(u.size() - 1, u.size() - 1);
+    jac = unotp.asDiagonal() + unot * (u.tail(1) - unotp.transpose());
+    Type out;
+    out = CppAD::log(jac.determinant());
+    return(out);
   }
 
 
