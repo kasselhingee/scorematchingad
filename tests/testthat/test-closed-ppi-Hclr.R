18a9ec1129aecb7098ea5f6bd146bdaa16fe9b74
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-01-16 03:38:02 +1100

    lots of learning about the clr estimators in the test file

--- tests/testthat/test-closed-ppi-Hclr.R
+++ tests/testthat/test-closed-ppi-Hclr.R
@@ -13,6 +13,14 @@ clr <- function(Y){
     return(mom - 1)
   }
 
+test_that("ppi() warns when bdrythreshold too low", {
+  set.seed(12345)
+  model <- ppi_egmodel(100, maxden = 4)
+
+  expect_warning(ppi(Y = model$sample, trans = "clr", bdrythreshold = 1E-10),
+                 "bdrythreshold")
+})
+
 test_that("Fitting Dirichlet via clr transform with fixed betap gets close to true values and other estimators for low concentrations", {
   skip_on_cran()
   set.seed(13412)
@@ -74,46 +82,14 @@ test_that("Fitting Dirichlet via clr transform gets close to true values and oth
   Y <- MCMCpack::rdirichlet(10000, beta0+1)
   out <- ppi(Y = Y,
              paramvec = ppi_paramvec(p = 3, AL=0, bL = 0),
-             trans = "clr", bdrythreshold = 0)
+             trans = "clr", bdrythreshold = 1E-6)
   out$est$beta
  
-  library(ggtern)
-  library(ggplot2)
-  library(dplyr)
-  colnames(Y) <- c("x", "y", "z")
-  Y %>%
-   as.data.frame() %>%
-   ggplot(aes(x=x, y = y, z = z)) +
-   coord_tern() +
-   geom_density_tern(bdl = 0.1, bdl.val = NA) +
-   geom_point(shape = "+")
-
-  clr(Y) %>%
-   as.data.frame() %>%
-   ggplot() +
-   geom_point(aes(x = x, y = y))
-
-  
   expect_absdiff_lte_v(out$est$beta, beta0, out$SE$beta * 3)
-  # but the estimate is wrong by 10 orders of magnitude
-  expect_lt(max(abs(out$SE$beta), na.rm = TRUE), 20)
+  expect_lt(max(abs(out$SE$beta), na.rm = TRUE), 1E-1)
 
-  # vs method of moments from JASA paper supplementary
-  dirichmom <- function(X) {
-    # Method of Moments estimates of the Dirichlet Distribution
-    temp <- dim(X); n <- temp[1]; m <- temp[2]
-    # X <- cbind(X,matrix(1-apply(X,1,sum)))
-    mom <- apply(X,2,mean)*(mean(X[,1])-mean(X[,1]^2))/(mean(X[,1]^2) - ((mean(X[,1]))^2))
-    return(matrix(mom))
-  }
-  dirichmom(Y)-1 #this is very accurate! :)
-
-  out2 <- ppi(Y = Y,
-             paramvec = ppi_paramvec(p = 3, AL=0, bL = 0),
-             trans = "sqrt", divweight = "minsq", acut = 0.01)
-
-  out2$est$beta
-
+  # and result close to dirichlet moment estimator
+  expect_absdiff_lte_v(out$est$beta, dirichmom(Y), c(0.01, 0.02, 0.01))
 })
 
 test_that("Fitting ppi via clr transform with fixed beta gets close to true values", {
@@ -130,13 +106,13 @@ test_that("Fitting ppi via clr transform with fixed beta gets close to true valu
 })
 
 
-test_that("Fitting ppi via clr transform with unfixed beta gets close to true values", {
+test_that("Fitting ppi all parameters via clr transform can get close to true values", {
   skip_on_cran()
   set.seed(12345)
-  model <- ppi_egmodel(1000000, maxden = 4)
+  model <- ppi_egmodel(10000, maxden = 4)
 
   out <- ppi(Y = model$sample,
-             trans = "clr")
+             trans = "clr", bdrythreshold = 1E-5)
 
   expect_absdiff_lte_v(out$est$paramvec, model$theta, out$SE$paramvec * 3)
   # and that the SE are small
@@ -144,7 +120,7 @@ test_that("Fitting ppi via clr transform with unfixed beta gets close to true va
 })
 
 
-test_that("Hess + Offset match gradient for Hclr", {
+test_that("Hess + Offset match gradient for Hclr in interior", {
   set.seed(1245)
   mod <- ppi_egmodel(100)
   Y <- mod$sample
@@ -153,6 +129,9 @@ test_that("Hess + Offset match gradient for Hclr", {
   tapes <- buildsmotape("Hclr", "ppi", utape = c(0.2, 0.3, 0.5), 
                         usertheta = ppi_paramvec(p = 3))
 
+  # find boundary points and remove them - expect results pJacobian to give good results when no points need approximation
+  isbdry <- simplex_isboundary(Y, 1E-5)
+  Y <- Y[!isbdry, ]
   values <- quadratictape_parts(tapes$smotape, Y)
 
   # expect results to match for gradient
@@ -163,16 +142,82 @@ test_that("Hess + Offset match gradient for Hclr", {
       t(values$offset[i, , drop = FALSE]))
   })
   gradpoly <- do.call(rbind, gradpoly)
-  expect_equal(gradorig, gradpoly)
+  expect_equal(gradorig, gradpoly, tolerance = 1E-5)
 })
 
-test_that("W is symmetric for ppi with clr, beta fixed", {
+test_that("Taylor approximations accuracy study", {
+  set.seed(1245)
+  mod <- ppi_egmodel(1000)
+  Y <- mod$sample
+  tapes <- buildsmotape("Hclr", "ppi", utape = c(0.2, 0.3, 0.5), 
+                        usertheta = ppi_paramvec(p = 3))
+
+  # find boundary points 
+  isbdry <- simplex_isboundary(Y, 1E-3)
+  Yapproxcentres <- Y
+  Yapproxcentres[!isbdry, ] <- NA
+  Yapproxcentres[isbdry, ] <- simplex_boundaryshift(Y[isbdry, , drop = FALSE], shiftsize = 1E-5)
+
+  approxedvals <- tape_smvalues(tapes$smotape, Y, mod$theta, xcentres = Yapproxcentres, approxorder = 10)
+  naivevals <- tape_smvalues(tapes$smotape, Y, mod$theta)
+
+  #compute distance to boundary
+  bdrydist <- apply(Y, MARGIN = 1, min)
+  bdrycomponent <- apply(Y, MARGIN = 1, which.min)
+
+  # look at obj values
+  library(dplyr)
+  library(ggplot2)
+  data.frame(naive = naivevals$obj,
+             approxed = approxedvals$obj,
+             bdrydist = bdrydist) %>%
+    ggplot(aes(x = bdrydist, y = abs(naive - approxed))) +
+    geom_point() +
+    scale_x_log10() +
+    scale_y_log10() + 
+    geom_smooth(method = "lm", se = FALSE)
+  # result is straight in log-log (i.e. power law)
+  fit <- lm(log(err) ~ log(bdrydist), data = data.frame(err = abs(naivevals$obj - approxedvals$obj)[isbdry, ], bdrydist = bdrydist[isbdry])) 
+  # about is err ~ exp(-40) / bdrydist^2
+  # reaches 1E-6 in error just below 1E-5 in bdry distance
+
+  # look at grad values
+  data.frame(err = abs(naivevals$grad-approxedvals$grad),
+           bdrydist = bdrydist,
+           bdrycomponent = factor(bdrycomponent)) %>%
+    tidyr::pivot_longer(!c(bdrydist, bdrycomponent)) %>%
+    ggplot(aes(x = bdrydist, y = value, col = bdrycomponent)) +
+    facet_wrap(vars(name)) +
+    geom_point() +
+    scale_x_log10() +
+    scale_y_log10() +
+    geom_smooth(method = "lm", se = FALSE)
+  # again straight lines in the log-log plots
+  # most components of the grad reach error of 1E-13 at about bdry dist of 1E-05  # the beta components are at this limit at 1E-3 bdry distance.
+  # I suspect the naive calculations are the one that are wrong here (since error is consistent relation to bdry distance) and that the cause is some numerical instability
+
+  # look at hess values
+  data.frame(err = abs(naivevals$hess-approxedvals$hess),
+           bdrydist = bdrydist,
+           bdrycomponent = factor(bdrycomponent)) %>%
+    tidyr::pivot_longer(!c(bdrydist, bdrycomponent)) %>%
+    ggplot(aes(x = bdrydist, y = value, col = bdrycomponent)) +
+    facet_wrap(vars(name)) +
+    geom_point() +
+    scale_x_log10() +
+    scale_y_log10()
+  # relationship between accuracy and distance (linear in log-log) is a power law for the errors above 1E-14.
+})
+
+test_that("W is symmetric for ppi with clr, fitting all parameters", {
   # if W is symmetric then should equal the smo up to a constant wrt theta
   # and my expectation is that W is symmetric for ppi with Hclr
   set.seed(1245)
   mod <- ppi_egmodel(100)
   Y <- mod$sample
   Y <- rbind(Y, rep(1/3, 3))
+  isbdry <- simplex_isboundary(Y, 1E-5)
+  Y <- Y[!isbdry, ]
 
   usertheta = ppi_paramvec(p = 3, beta = mod$beta)
   ftheta <- t_ut2f(usertheta, mod$theta)
