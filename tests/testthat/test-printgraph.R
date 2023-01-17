feea91573c3289774738dd11e631afb27737f22d
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-01-17 18:31:39 +1100

    printgraph() is workinggit mv tests/testthat/test-pprintgraph.R tests/testthat/test-printgraph.R

--- tests/testthat/test-printgraph.R
+++ tests/testthat/test-printgraph.R
@@ -3,6 +3,6 @@ test_that("Printing CppAD graph of tape works for dirichlet on simplex", {
   beta <- c(-0.8, -0.5, 1)
   pman <- pmanifold("simplex")
   pppi <- ptapell(rep(0.1, length(beta)), beta, llname = "dirichlet", pman,
-                  fixedtheta = rep(FALSE, length(beta)), verbose = TRUE)
-  expect_error(pprintgraph(pppi))
+                  fixedtheta = rep(FALSE, length(beta)), verbose = FALSE)
+  printgraph(pppi)
 })
