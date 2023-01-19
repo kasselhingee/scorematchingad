813cd1e8c8ae715aa1a1ce84e75be94ec06560ff
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-01-19 15:13:38 +1100

    turning printgraph test into actual testthattest

--- tests/testthat/test-printgraph.R
+++ tests/testthat/test-printgraph.R
@@ -1,8 +1,9 @@
 
-test_that("Printing CppAD graph of tape works for dirichlet on simplex", {
+test_that("Printing CppAD graph of tape runs for dirichlet on simplex", {
   beta <- c(-0.8, -0.5, 1)
   pman <- pmanifold("simplex")
   pppi <- ptapell(rep(0.1, length(beta)), beta, llname = "dirichlet", pman,
                   fixedtheta = rep(FALSE, length(beta)), verbose = FALSE)
-  printgraph(pppi)
+  expect_output(printgraph(pppi), regex = "mul.*log")
 })
+
