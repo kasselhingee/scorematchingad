67761ecdf0517ef5f4b1be18fb08bccfac61b7ce
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-02-06 11:36:29 +1100

    externtest includes comments for quickly checking examples

--- tests/externtest.R
+++ tests/externtest.R
@@ -2,3 +2,6 @@ devtools::install(upgrade = "never", quick = TRUE)
 Sys.setenv(NOT_CRAN = "true")
 testthat::test_dir("./tests/testthat/", load_package = "installed", package = "scorecompdir")
 
+# test examples
+#library("scorecompdir")
+#testthat::test_examples(".")
