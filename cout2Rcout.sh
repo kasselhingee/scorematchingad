d13fbacd325698d4f9d55128d1d57402c54328a1
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-02-08 14:11:52 +1100

    attempts at full script

--- cout2Rcout.sh
+++ cout2Rcout.sh
@@ -8,10 +8,10 @@ sed -i "s/ cerr/ Rcerr/g" inst/include/cppad/*/*.hpp
 # the above is sufficient for avoiding check notes on 20230208, I suspect the rest of CPPAD isn't used yet
 
 # to do it for everything:
-#sed -i "/^# include.*/i # include <RcppCommon.h>" inst/include/cppad/*.hpp
-#sed -i "s/std::cout/Rcpp::Rcout/g" | find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)
-#sed -i "s/std::cerr/Rcpp::Rcerr/g" | find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)
-sed -i "s/ cout/ Rcout/g" | | find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)
-sed -i "s/ cerr/ Rcerr/g" | find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)
+sed -i "0,/^# include.*/ //i # include <RcppCommon.h>" inst/include/cppad/*.hpp
+sed -i "s/std::cout/Rcpp::Rcout/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)`
+sed -i "s/std::cerr/Rcpp::Rcerr/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)`
+sed -i "s/ cout/ Rcout/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)`
+sed -i "s/ cerr/ Rcerr/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)`
 
 
