f2e4f9b5a39b2a63db6d2776a2de19108b598411
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-02-08 15:08:23 +1100

    script doesn't alter .omh files (which I think are doc files)

--- cout2Rcout.sh
+++ cout2Rcout.sh
@@ -1,13 +1,11 @@
 # to do it for everything:
-# add include RcppCommon
-sed -i "0,/^# include.*/s//# include <RcppCommon.h>\n&/" inst/include/cppad/*.hpp
 
-# replace things: 
-sed -i "s/std::cout/Rcpp::Rcout/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)`
-sed -i "s/std::cerr/Rcpp::Rcerr/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)`
-sed -i "s/ cout/ Rcout/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)`
-sed -i "s/ cerr/ Rcerr/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)`
+# replace std::cout and std::cerr: 
+sed -i "s/std::cout/Rcpp::Rcout/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" \)`
+sed -i "s/std::cerr/Rcpp::Rcerr/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" \)`
+sed -i "s/ cout/ Rcout/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" \)`
+sed -i "s/ cerr/ Rcerr/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" \)`
 
 # add include RcppCommon to any file that uses Rcpp
-sed -i "0,/^# include.*/s//# include <RcppCommon.h>\n&/" `grep -l "Rcpp" inst/include/cppad/utility/*.hpp`
+sed -i "0,/^# include.*/s//# include <RcppCommon.h>\n&/" `grep -l "Rcpp" inst/include/cppad/* -r`
 
