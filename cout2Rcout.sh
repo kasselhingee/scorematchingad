633b8dd388e7a3227acb9cba7ab593a769b48bda
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-02-08 13:59:45 +1100

    draft script to replace all of std::cout and std::cerr

---
+++ cout2Rcout.sh
@@ -0,0 +1,17 @@
+# add include RcppCommon to cppad.hpp
+sed -i "/^# include.*/i # include <RcppCommon.h>" inst/include/cppad/cppad.hpp
+# then use it instead of std::cout
+sed -i "s/std::cout/Rcpp::Rcout/g" inst/include/cppad/core/*.hpp inst/include/cppad/core/forward/*.omh
+sed -i "s/std::cerr/Rcpp::Rcerr/g" inst/include/cppad/core/*.hpp
+sed -i "s/std::cerr/Rcpp::Rcerr/g" inst/include/cppad/*/*.hpp
+sed -i "s/ cerr/ Rcerr/g" inst/include/cppad/*/*.hpp
+# the above is sufficient for avoiding check notes on 20230208, I suspect the rest of CPPAD isn't used yet
+
+# to do it for everything:
+#sed -i "/^# include.*/i # include <RcppCommon.h>" inst/include/cppad/*.hpp
+#sed -i "s/std::cout/Rcpp::Rcout/g" | find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)
+#sed -i "s/std::cerr/Rcpp::Rcerr/g" | find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)
+sed -i "s/ cout/ Rcout/g" | | find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)
+sed -i "s/ cerr/ Rcerr/g" | find inst/include/cppad/ -type f \( -iname "*.hpp" -o -iname "*.omh" \)
+
+
