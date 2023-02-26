c91fa6d33cbe1c7db51a2e41c24ef9b0609f410d
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-02-27 10:02:53 +1100

    better help for manifoldtransform

--- R/manifoldtransform.R
+++ R/manifoldtransform.R
@@ -1,20 +1,33 @@
 #' @title Build an object specifying the manifold-transform pair.
 #' @description Generate an object used to specify a manifold and transformation to the manifold. This specific to each type of data.
-#' @param name Name of the manifold-transform pair. See details.
-#' @return An `Rcpp::XPtr` object with attribute `name`.
+#' @param start The starting manifold. Used for checking that `tran` and `man` match.
+#' @param tran The name of a transformation. Available transformations are
+#'  + `srqt'
+#'  + `alr'
+#'  + `clr'
+#'  + `none' (or 'identity')
+#' @param man The name of the manifold that `tran` maps `start` to. Available manifolds are:
+#'  + `sph' unit sphere
+#'  + `Hn111' hyperplane normal to 1, 1, 1, 1, ...
+#'  + `sim' simplex
+#'  + `Euc' Euclidean space
+#' @return A named list with:
+#'  + `tran` A object of type `Rcpp_transform_ad` representing the transform
+#'  + `man` A object of type `Rcpp_man_ad` representing the manifold
 #' @details
-#' Available pairs are
-#'  + "sphere" for square-root transformation from the simplex to the positive orthant of the sphere
-#'  + "simplex" for the simplex without any transformation.
-#'  + "Ralr" for the additive log-ratio transformation from the simplex to Euclidean space, using the final component of vectors in the denominator of the ratio.
-#'  + "Snative" for the sphere without any transformation
+#' Only some combinations of `start`, `tran` and `man` are available because `tran` must map between `start` and `man`.
+#' These combinations of `start`-`tran`-`man` are available:
+#'
+#' ```{r mantrans, results = "asis", echo = FALSE}
+#' cat(paste(" +", mantrancombos), sep = "\n")
+#' ```
 #' @examples
-#' manifoldtransform("alr", "Euc")
+#' manifoldtransform("sim", "alr", "Euc")
 #' @export
 manifoldtransform <- function(start, tran, man){
   stopifnot(paste(start, tran, man, sep = "-") %in% mantrancombos)
-  out <- list(tran = new(mantranmodule$transform_ad, tran),
-       man = new(mantranmodule$man_ad, man))
+  out <- list(tran = methods::new(mantranmodule$transform_ad, tran),
+       man = methods::new(mantranmodule$man_ad, man))
   return(out)
 }
 
