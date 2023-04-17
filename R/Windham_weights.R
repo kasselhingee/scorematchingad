e8f5086a855f82378ddc0e40c24165763c68dfd0
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-04-17 15:07:02 +1000

    bug fix

--- R/Windham_weights.R
+++ R/Windham_weights.R
@@ -16,13 +16,13 @@ Windham_weights <- function(ldenfun, Y, theta, cW){
   stopifnot(is.numeric(cW))
   thetaforweights <- cW * theta #the elements of theta with FALSE inWW will be set to zero
   weights <- exp(ldenfun(Y = Y, theta = thetaforweights))
+  weights=weights/sum(weights)
   if (any(!is.finite(weights))){#when lden values too huge for numerics, set them to the max non-Inf value
     stop(paste0("Non-finite weights for observations: ", 
                paste(which(!is.finite(weights)), collapse = ", "), " at theta of ",
                paste(theta, collapse = " "),
                ". Try a different weight vector 'cW', a different 'paramvec_start' or a different fixed point search method."))
   }
-  weights=weights/sum(weights)
   return(weights)
 }
 
