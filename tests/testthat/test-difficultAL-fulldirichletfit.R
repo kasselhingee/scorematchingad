229cd92a8cbabe19fe9e0febccb621ad3211873b
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-02-20 17:44:02 +1100

    speed up of a test

--- tests/testthat/test-difficultAL-fulldirichletfit.R
+++ tests/testthat/test-difficultAL-fulldirichletfit.R
@@ -5,8 +5,8 @@ test_that("full ppi estimates are mostly within 3 SE for difficult AL with large
   ALs <- exp(rsymmetricmatrix(p-1, -4, 4))
   bL <- rep(0, p-1)
   beta <- c(-0.7, -0.8, -0.3, 0, 0)
-  set.seed(1111) #this seed leads to some ginormous elements for the second diagonal element of ALs
-  suppressWarnings(prop <- rppi(1000, beta = beta, AL=ALs, bL=bL, maxden=35)) #rppi_singly took 1005 seconds, rppi() took 13seconds
+  set.seed(1111)
+  suppressWarnings(prop <- rppi(500, beta = beta, AL=ALs, bL=bL, maxden=35)) #rppi_singly took 1005 seconds, rppi() took 13seconds
   #prop %>% as_tibble() %>% tidyr::pivot_longer(everything()) %>% ggplot() + facet_wrap(vars(name)) + geom_freqpoly(aes(x=value))
 
   est_cppad <- ppi(prop, ppi_paramvec(bL = bL), trans = "sqrt", divweight = "minsq",
