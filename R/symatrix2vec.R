82a33a7a94608b1b58746f912b8aa91cc29eff8d
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-02-22 14:38:37 +1100

    deleted combparam2uppertriorder_matrix

--- R/symatrix2vec.R
+++ R/symatrix2vec.R
@@ -35,31 +35,3 @@ indexcombinations <- function(d){
   return(list(ind = ind, qind = qind))
 }
 
-# for reordering a parameter vector created from a matrix using utils::combn
-#ltheta is the length of the parameter vector defining the matrix
-combparam2uppertriorder_matrix <- function(ltheta){
-  # l(theta) = p-1 + (p-1) * (p-2)/2
-  # = p-1 + (p^2 -p -2p + 2)/2
-  # 2l = 2p-2 + p^2 - 3p + 2
-  # 0 = p^2  - p - 2l
-  # p = (1 pm sqrt(1 - 4*(-2l)))/2
-  #   = (1 pm sqrt(8l + 1)) /2
-  #   = (1+sqrt(8l + 1) / 2
-  p <- (1+sqrt(8 * ltheta + 1)) / 2
-  sp <- p-1
-
-  #upper.tri ordering
-  AL <- matrix(NA, sp, sp)
-  diag(AL) <- 1:sp
-  AL[upper.tri(AL)] <- sp + (1:sum(upper.tri(AL)))
-  AL[lower.tri(AL)] <- t(AL)[lower.tri(AL)]
-
-  #comb ordering
-  inthetaorder <- c(diag(AL),
-                    AL[t(utils::combn(1:(p-1), 2))]) #each column is the own dimension, each row single element to extract
-
-  stopifnot(length(inthetaorder) == ltheta) #check that length produced is correct
-  stopifnot(all(inthetaorder[inthetaorder] == seq(1, length.out = ltheta))) #check that ordering index is correct
-  return(inthetaorder)
-}
-
