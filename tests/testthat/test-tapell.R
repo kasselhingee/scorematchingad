9e91861c255ccd0cec9ed6830434cc783045a3ef
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-02-22 14:50:05 +1100

    checking verbose methods for tapell

---
+++ tests/testthat/test-tapell.R
@@ -0,0 +1,17 @@
+test_that("tapell generates correct objects", {
+maninfo <- manifoldtransform("sim", "sqrt", "sph")
+ppitape <- tapell(llname = "ppi",
+                  ytape = c(0.2, 0.3, 0.5),
+                  usertheta = ppi_paramvec(p = 3), 
+                  tran = maninfo$tran) 
+expect_true(R6::is.R6(ppitape))
+
+# and verbose works too
+expect_output({ppitape <- tapell(llname = "ppi",
+                  ytape = c(0.2, 0.3, 0.5),
+                  usertheta = ppi_paramvec(p = 3), 
+                  tran = maninfo$tran,
+                  verbose = TRUE)},
+              "pattern.*tape.*dynamic")
+expect_true(R6::is.R6(ppitape))
+})
