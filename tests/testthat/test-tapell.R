8b42abc571932ee85c90779470d5f52256769230
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-02-22 14:52:11 +1100

    test tweak

--- tests/testthat/test-tapell.R
+++ tests/testthat/test-tapell.R
@@ -14,4 +14,9 @@ expect_output({ppitape <- tapell(llname = "ppi",
                   verbose = TRUE)},
               "pattern.*tape.*dynamic")
 expect_true(R6::is.R6(ppitape))
+
+expect_error({tapell(llname = "error",
+                  ytape = c(0.2, 0.3, 0.5),
+                  usertheta = ppi_paramvec(p = 3), 
+                  tran = maninfo$tran)}, "function")
 })
