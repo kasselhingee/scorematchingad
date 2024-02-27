a8c0c5a4b2f6a64bdb95a0d86ae78a17b65f955d
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-02-27 20:00:25 +1100

    help polishes for buildsmotape

--- R/buildsmotape.R
+++ R/buildsmotape.R
@@ -6,16 +6,17 @@
 #' The function `buildsmotape()` generates `CppAD` tapes (called `ADFun`) for the log-likelihood (without normalising constant) and the score matching discrepancy function \eqn{A(z) + B(z) + C(z)} from `vignette("scorematchingintro")` for a specified model faimly.
 #' Three steps are performed by `buildsmotape()`: first an object that specifies the manifold and any transformation to another manifold is created using [`manifoldtransform()`]; then a tape of the log-likelihood (without normalising constant) is created using [`tapell()`]; finally a tape of \eqn{A(z) + B(z) + C(z)} is created using [`tapesmo()`].
 #' @details
-#' The model log-likelihood must be implemented in `C++` and is selected by name. Similarly the transforms of the manifold must be implemented in `C++` and selected by name.
+#' The model log-likelihood without normalising constant must be implemented in `C++` and is selected by name. Similarly the transforms of the manifold must be implemented in `C++` and selected by name.
 #'
 #' When using, `CppAD` one first creates *tapes* of functions. These tapes can then be used for evaluating the function and its derivatives, and generating further tapes through argument swapping, differentiation and composition.
 #' The taping relies on specifying typical argument values for the functions, so the programming is simplest when the function is defined without conditions.
 #' Tapes can have both *independent* variables and *dynamic* variables.
 #' The differentiation with `CppAD` occurs with respect to the independent variables.
-#' Tapes of tapes are possible, including tapes that swap the independent and dynamic variables - this is how this package differentiates with respect to a dynamic variables.
+#' Tapes of tapes are possible, including tapes that swap the independent and dynamic variables - this is how this package differentiates with respect to a dynamic variables (see [`swapDynamic()`]).
 #'
-#' To build a tape for the score matching discrepancy function, the package first tapes the map from a point \eqn{z} on the manifold to the value of the log-likelihood, where the independent variable is the \eqn{z}, and the dynamic variable is a vector of the *non*-fixed parameter values.
-#' This tape is then used to generate a tape for the score matching discrepancy with the non-fixed parameter values as the independent variable.
+#' To build a tape for the score matching discrepancy function, the package first tapes the map from a point \eqn{z} on the `end` manifold to the value of the improper log-likelihood, where the independent variable is the \eqn{z}, and the dynamic variable is a vector of the parameters to estimate, and remaining parameters are fixed and not estimated.
+#' This tape is then used to generate a tape for the score matching discrepancy with the parameters to estimate are the independent variable.
+#' 
 #' # Introduction to CppAD Tapes
 #' This package uses version 2022000.2 of the algorithmic differentiation library `CppAD` \insertCite{bell2023cp}{scorecompdir} to build score matching estimators.
 #' Full help for `CppAD` can be found at <https://cppad.readthedocs.io/>.
@@ -26,6 +27,7 @@
 #' The result of taping is an [`ADFun`](https://cppad.readthedocs.io/en/latest/ADFun.html) object, often called a *tape*.
 #' This `ADFun` object can be evaluated, differentiated, used for further taping (see [base2ad](https://cppad.readthedocs.io/en/latest/base2ad.html)), solving differential equations and more.
 #' The differentiation is with respect to the independent variables, however the dynamic parameters can be altered which allows for creating a new `ADFun` object where the dynamic parameters have become independent variables (see [`tapeSwap()`]).
+#' For the purposes of score matching, there is a third type of parameter: the *fixed* parameters, which are the elements of the model's parameter vector that are given and not estimated.
 #' 
 #' # Warning: multiple CPU
 #' Each time computations such as derivatives are performed the corresponding `C++` object is altered. Parallel use of the same `ADFun` object thus requires care and is not tested. For now I recommend creating a new `ADFun` object for each CPU.
@@ -33,7 +35,10 @@
 #' @references \insertAllCited{}
 
 #' @return
-#' `buildsmotape()` returns a list of the log-likelihood tape (note that the *input* for this tape is a function on the manifold), the tape of the score matching discrepancy (the *input* here is the non-fixed parameter values), and some information used to generate the tape.
+#' `buildsmotape()` returns a list of:
+#'   + an [`ADFun`] object containing a log-likelihood tape with \eqn{z} on the `end` manifold as the independent variable
+#'   + an [`ADFun`] object containing a tape of the score matching discrepancy with the non-fixed parameter values as the independent variables
+#'   + some information used to generate the tapes
 #'
 #' 
 #' @examples
