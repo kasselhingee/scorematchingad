7622e96944292e18a8da3aa759be0db490abcedf
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-03-13 08:03:23 +1100

    when using factory for Rcpp module it seems like deleting the returned pointer isnt needed

--- src/exposemanifold.cpp
+++ src/exposemanifold.cpp
@@ -1,10 +1,10 @@
 # include "exposemanifold.h"
 
-// manifold object 'factory'
+// manifold object 'factory' . Per Rcpp instructions it returns a pointer so new must be used. It looks like delete isnt mentioned with factory even in the examples
 manifold<a1type> * newmanifold(const std::string &manifoldname){
   manifold<a1type> * out;  //returning a pointer
   if (manifoldname.compare("sph") == 0){
-    out = new mantran::sph<a1type>();
+    out = new mantran::sph<a1type>(); //new needed because its going out of scope when returned by the function
   } else if (manifoldname.compare("sim") == 0){
     out = new mantran::sim<a1type>();
   } else if (manifoldname.compare("Euc") == 0){
@@ -16,11 +16,6 @@ manifold<a1type> * newmanifold(const std::string &manifoldname){
   }
 
   return(out);
-}
-
-// manifold object finalizer (aka memory free-er)
-void delmanifold(manifold<a1type> * ptr){
-  delete ptr;
 }
 
 // transform factory
@@ -41,18 +36,12 @@ transform<a1type> * newtransform(const std::string &name){
   return(out);
 }
 
-// transform object finalizer (aka memory free-er)
-void deltransform(transform<a1type> * ptr){
-  delete ptr;
-}
-
 RCPP_MODULE(manifolds) {
   Rcpp::class_< manifold_a1type >("man_ad") //manifold_a1type is a synonym with manifold<a1type> due to typedef in scorecompdir_types.h
       .factory<const std::string &>(newmanifold)
       .method("Pmatfun", &manifold_a1type::Pmatfun, "Pmatfun(z) returns the matrix that orthogonally projects onto the manifold's tangent space at z")
       .method("dPmatfun", &manifold_a1type::dPmatfun, "dPmatfun(z, i) returns the element-wise derivative of Pmatfun() at location z with respect to the ith dimension")
       .method("name", &manifold_a1type::name)
-      .finalizer(&delmanifold)
   ;
   
   Rcpp::class_< transform_a1type >("transform_ad") //transform_a1type is synonym with transform<a1type> due to typedef in  scorecompdir_types.h
@@ -61,7 +50,6 @@ RCPP_MODULE(manifolds) {
       .method("fromM", &transform_a1type::fromM, "reverse of toM()")
       .method("logdetJfromM", &transform_a1type::logdetJfromM, "compute the log of the determinant of the Jacobian of fromM()")
       .method("name", &transform_a1type::name)
-      .finalizer(&deltransform)
   ;
 }
 
