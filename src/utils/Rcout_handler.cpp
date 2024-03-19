c901be9bc4a6a195c00ff5d19bc0aa73db542ccb
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-03-19 20:08:34 +1100

    Revert "cant get Rcout_handler to compile - I suspect a newby C++ thing"
    This reverts commit 315a25f860560ac8598773d19297ef7c98fe882c.

--- src/utils/Rcout_handler.cpp
+++ src/utils/Rcout_handler.cpp
@@ -1,11 +1,11 @@
-# include "Rcout_handler.h"
+# include Rcout_handler.h
 
-void Rcout_handler(
-   const bool known,
-   const int  line,
-   const char *file,
-   const char *exp,
-   const char *msg)
+void myhandler(
+   bool known       ,
+   int  line        ,
+   const char *file ,
+   const char *exp  ,
+   const char *msg  )
 {  // error handler must not return, so throw an exception
    Rcpp::Rcout << "Rcout: " << line;
 }
--- src/utils/Rcout_handler.h
+++ src/utils/Rcout_handler.h
@@ -1,11 +1,13 @@
 #ifndef Rcout_handler
 #define Rcout_handler
 
-# include <cppad/utility/error_handler.hpp>
-# include <cstring>
-
 // for replacing cppad errors with Rcpp::Rcout
 
-void Rcout_handler(const bool known, const int  line, const char *file , const char *exp, const char *msg);
+void Rcout_handler(
+   bool known       ,
+   int  line        ,
+   const char *file ,
+   const char *exp  ,
+   const char *msg  );
 
 #endif
