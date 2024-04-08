a29b488b295924c4f1a6c32efdf217c9f2f61243
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-04-08 23:19:31 +1000

    still not finding scorematchingad.h

--- R/inlineCxxPlugin.R
+++ R/inlineCxxPlugin.R
@@ -2,12 +2,12 @@
 inlineCxxPlugin <- function(...) {
     plugin <-
         Rcpp::Rcpp.plugin.maker(
-                  include.before = c("#include <cppad/cppad.hpp>", "#include <RcppEigen.h>", '#include <scorematchingad.h>'),
+                  include.before = c('#include <scorematchingad.h>'),
                   package        = "scorematchingad",
-                  Makevars = "PKG_CXXFLAGS = -I../inst/include",
                   LinkingTo = c("RcppEigen", "scorematchingad", "Rcpp")
               )
     settings <- plugin()
+    settings$env$PKG_CPPFLAGS <- "-I../inst/include"
     settings
 }
 
