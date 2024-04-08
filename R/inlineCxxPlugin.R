ffb2ddebd45e0d4de8beabcfcefbc5133acc24cf
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-04-08 23:09:05 +1000

    all unit tests pass, except the inline one - I still can't get it to find 'scorematchingad.h'

--- R/inlineCxxPlugin.R
+++ R/inlineCxxPlugin.R
@@ -2,8 +2,9 @@
 inlineCxxPlugin <- function(...) {
     plugin <-
         Rcpp::Rcpp.plugin.maker(
-                  include.before = c("#include <cppad/cppad.hpp>", "#include <RcppEigen.h>"),
+                  include.before = c("#include <cppad/cppad.hpp>", "#include <RcppEigen.h>", '#include <scorematchingad.h>'),
                   package        = "scorematchingad",
+                  Makevars = "PKG_CXXFLAGS = -I../inst/include",
                   LinkingTo = c("RcppEigen", "scorematchingad", "Rcpp")
               )
     settings <- plugin()
