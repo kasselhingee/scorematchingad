ad9b24ede2cd2fa8017ee578891e456eb5bf32c2
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-04-09 18:52:46 +1000

    cppFunction() and cppXPtr() is working for RcppEigen stuff

--- tests/testthat/test-inlineCxxPlugin.R
+++ tests/testthat/test-inlineCxxPlugin.R
@@ -10,11 +10,57 @@ test_that("simple cxx compiles and runs", {
   #write(strsplit(fx@code, "\n")[[1]], file = "tmp.cpp")
 })
 
-test_that("cxx using my code compiles and runs", {
-  expect_no_error({fx <- inline::cxxfunction(signature(x = "integer", y = "numeric"), 
-"manifold<a1type> out();
-return wrap( as<int>(x) * as<double>(y));",
-                             plugin = "scorematchingad" )})
+test_that("Rcpp::cppFunction() accesses Eigen and wraps automatically", {
+# Rcpp::cppFunction() allows for automatic use of wrap and as!
+# note it is using the inline plugin
+fxptr <- Rcpp::cppFunction("
+double foo(Eigen::Matrix<double, Eigen::Dynamic, 1> x) {
+ double out;
+ out = x.sum();
+ return out;
+}
+", depends = c("RcppEigen"))
+  expect_equal(fxptr(c(1, 2)), 3)
+})
+
+
+test_that("RcppXPtrUtils accesses Eigen and wraps automatically", {
+# Rcpp::cppFunction() allows for automatic use of wrap and as!
+# note it is using the inline plugin
+ptr <- RcppXPtrUtils::cppXPtr("
+double foo(Eigen::Matrix<double, Eigen::Dynamic, 1> x) {
+ double out;
+ out = x.sum();
+ return out;
+}
+", depends = c("RcppEigen"), verbose = TRUE) |> expect_no_error()
+  # can't checkXPtr because argument type has commas in it
+})
+
+
+Rcpp::cppFunction("
+veca1 foo(transform<a1type> & tran, veca1 z) {
+ veca1 u(0);
+ //u = tran.fromM(z);
+ return u;
+}
+", depends = c("RcppEigen", "scorematchingad"))
+
+
+#RcppXPtrUtils looks like exactly what I want
+
+test_that("returning pointer can be used elsewhere", {
+  expect_no_error({
+fx <- inline::cxxfunction(
+signature(z_ad = "numeric", theta_ad = "numeric", llname = "string", 
+body = "
+
+a1type (*ll)(const veca1 &, const veca1 &) = nullptr;
+ll = ll::ll_ppi;
+return Rcpp::XPtr< CppAD::ADFun<double> > pout(out, true);
+",
+                             plugin = "scorematchingad" )
+})
   expect_no_error(fx(2L, 5))
   #write(strsplit(fx@code, "\n")[[1]], file = "tmp.cpp")
 })
