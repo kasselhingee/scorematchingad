2e5cd608da9d699624c682fc1134b7b216d489d3
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-04-09 12:37:49 +1000

    declaring and defining my custom manifold objects works

--- tests/testthat/test-inlineCxxPlugin.R
+++ tests/testthat/test-inlineCxxPlugin.R
@@ -10,3 +10,11 @@ test_that("simple cxx compiles and runs", {
   #write(strsplit(fx@code, "\n")[[1]], file = "tmp.cpp")
 })
 
+test_that("cxx using my code compiles and runs", {
+  expect_no_error({fx <- inline::cxxfunction(signature(x = "integer", y = "numeric"), 
+"manifold<a1type> out();
+return wrap( as<int>(x) * as<double>(y));",
+                             plugin = "scorematchingad" )})
+  expect_no_error(fx(2L, 5))
+  #write(strsplit(fx@code, "\n")[[1]], file = "tmp.cpp")
+})
