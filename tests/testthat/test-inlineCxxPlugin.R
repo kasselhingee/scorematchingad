96cd555bf64e433e9febe0eb199353fad254b3b3
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-04-09 12:17:42 +1000

    plugin is automatically found, no 'register' plugin required

--- tests/testthat/test-inlineCxxPlugin.R
+++ tests/testthat/test-inlineCxxPlugin.R
@@ -1,10 +1,8 @@
 test_that("getting inline plugin passes", {
-  inline::registerPlugin("scorematchingad", inlineCxxPlugin)
   expect_no_error(inline::getPlugin("scorematchingad"))
 })
 
 test_that("simple cxx compiles and runs", {
-  inline::registerPlugin("scorematchingad", inlineCxxPlugin)
   expect_no_error({fx <- inline::cxxfunction(signature(x = "integer", y = "numeric"), 
                              "return wrap( as<int>(x) * as<double>(y));",
                              plugin = "scorematchingad" )})
