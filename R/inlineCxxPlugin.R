6d693d1808d6cd027bc14e077ee5596295ea72cf
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-04-29 16:33:13 +1000

    plugin bugs fixed

--- R/inlineCxxPlugin.R
+++ R/inlineCxxPlugin.R
@@ -3,7 +3,7 @@ inlineCxxPlugin <- function(...) {
     plugin <-
         Rcpp::Rcpp.plugin.maker(
                   include.before = c('#include <scorematchingad.h>'),
-                  include.after = paste("#include", c("<utils/PrintFor.hpp>", "<tapell.cpp>", "<utils/wrapas.hpp>", "<manifoldtransforms/transforms.hpp>", "<likelihoods/likelihoods.hpp>")),
+                  include.after = paste("#include", c("<utils/PrintFor.hpp>", "<tapellcpp.h>", "<utils/wrapas.hpp>", "<manifoldtransforms/transforms.hpp>", "<likelihoods/likelihoods.hpp>"), collapse = "\n"),
                   package        = "scorematchingad",
                   LinkingTo = c("RcppEigen", "scorematchingad", "Rcpp")
               )
