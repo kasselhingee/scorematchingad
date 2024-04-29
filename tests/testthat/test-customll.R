aeadd29d28826b553db789d1ed14118373b33bc9
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-04-29 10:23:23 +1000

    tapellcpp inside cppFunction is working on linux: couldn't see how to get g++ to compile a .cpp in inst/include, so content moved to the .h, then needed to include utils/wrapas.hpp for input and output of veca1 etc

--- tests/testthat/test-customll.R
+++ tests/testthat/test-customll.R
@@ -30,3 +30,42 @@ a1type dirichlet(const vecd &u, const veca1 &beta) {
 }")})
 })
 
+test_that("cppFunction that does tapellcpp() of a custom ll works on Windows", {
+  myfun <- Rcpp::cppFunction(
+depends =  c("RcppEigen", "scorematchingad", "Rcpp"),
+includes = c("#include <utils/PrintFor.hpp>", "#include <tapellcpp.h>", "#include <manifoldtransforms/transforms.hpp>", "#include <utils/wrapas.hpp>"),
+verbose = TRUE,
+code = "
+  Rcpp::XPtr< CppAD::ADFun<double> > tapedirichlet(veca1 z, veca1 theta, transform_a1type & tran, Eigen::Matrix<int, Eigen::Dynamic, 1> fixedtheta, bool verbose);
+
+  a1type dirichlet(const veca1 &u, const veca1 &beta) {
+          PrintForVec(\"u is:\", u);
+          size_t d  = u.size();
+          a1type y(0.);  // initialize summation
+          for(size_t i = 0; i < d; i++)
+          {   y   += beta[i] * log(u[i]);
+          }
+          return y;
+  }
+
+  Rcpp::XPtr< CppAD::ADFun<double> > tapedirichlet(veca1 z, veca1 theta, transform_a1type & tran, Eigen::Matrix<int, Eigen::Dynamic, 1> fixedtheta, bool verbose){
+  CppAD::ADFun<double>* out = new CppAD::ADFun<double>;
+  *out = tapellcpp(z,
+                theta,
+                *dirichlet,
+                tran,
+                fixedtheta,
+                verbose);
+
+  Rcpp::XPtr< CppAD::ADFun<double> > pout(out, true);
+  return(pout);
+  }
+  ")
+
+  maninfo <- manifoldtransform("sim", "identity", "sim")
+  mytape <- myfun(c(0.1, 0.4, 0.5), c(1,1,1), maninfo$tran, rep(FALSE, 3), verbose = TRUE)
+
+  expect_equal(pForward0(mytape, rep(1/3, 3), rep(-0.5, 3)), 3 * (-0.5 * log(1/3)))
+  expect_equal(pJacobian(mytape, rep(1/3, 3), rep(-0.5, 3)), rep(-0.5 * 3, 3))
+
+})
