435e07d15e1985ea561612907b2ba31b185bcaa7
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-05-02 09:46:26 +1000

    inlineCxxPlugin includes more of scorematchingad code

--- R/inlineCxxPlugin.R
+++ R/inlineCxxPlugin.R
@@ -2,7 +2,8 @@
 inlineCxxPlugin <- function(...) {
     plugin <-
         Rcpp::Rcpp.plugin.maker(
-                  include.before = c('#include "scorematchingad.h"'),
+                  include.before = c('#include <scorematchingad.h>'),
+                  include.after = paste("#include", c("<utils/PrintFor.hpp>", "<utils/wrapas.hpp>", "<likelihoods/likelihoods.hpp>"), collapse = "\n"),
                   package        = "scorematchingad",
                   LinkingTo = c("RcppEigen", "scorematchingad", "Rcpp")
               )
