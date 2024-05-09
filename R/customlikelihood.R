621c5e58413c88e565df75830a4eb6ac13858788
kasselhingee <12227692+kasselhingee@users.noreply.github.com>
2024-05-10 08:27:03 +1000

    Updates to help

--- R/customlikelihood.R
+++ R/customlikelihood.R
@@ -7,14 +7,14 @@
 #' @param verbose passed to [`Rcpp::cppFunction()`].
 #' @param code `C++` code for a log-likehood function (with normalising constant omitted if desired). See details for more.
 #' @description
-#' In a linux operating platform using the `gcc` compiler, supply `C++` code to specify a custom log-likelihood, much like `TMB::compile()` is passed `C++` code that formulate models.
+#' Supply `C++` code to specify a custom log-likelihood, much like `TMB::compile()` is passed `C++` code that formulate models.
 #' For score matching the normalising constant of the log-likelihood can be omitted.
-#' Taping of the function currently only works with the `gcc` compiler. You can see if taping works by running `customll_test()`.
+#' Taping of the function currently only works with the `gcc` compiler, typically on Linux; taping works if `customll_test()` returns `TRUE`.
 #' @details
 #' The function uses [`RcppXPtrUtils::cppXPtr()`] and [`Rcpp::cppFunction()`]. 
 #' It is good practice to check the returned object using [`evalll()`].
 #' The first compilation in a session can be very slow.
-#' # code 
+#' # Code Argument
 #' `code` must be `C++` that uses only `CppAD` and `Eigen`, which makes it very similar to the requirements of the input to `TMB::compile()` (which also uses `CppAD` and `Eigen`).
 #' 
 #' The start of `code` should always be "`a1type fname(const veca1 &x, const veca1 &theta){`" where `fname` is your chosen name of the log-likelihood function, `x` represents a point in the data space and `theta` is a vector of parameters for the log-likelihood. This specifies that the function will have two vector arguments (of type `veca1`) and will return a single numeric value (`a1type`).
@@ -26,6 +26,8 @@
 #' See the [Eigen documentation](https://eigen.tuxfamily.org/dox/group__QuickRefPage.html) for quick reference to available operations from Eigen. Limited operations are available directly from `CppAD` without `Eigen`: [unary operations](https://cppad.readthedocs.io/latest/unary_standard_math.html) and [binary operations](https://cppad.readthedocs.io/latest/binary_math.html). 
 #' For the purposes of score matching the operations should all be smooth to create a smooth log-likelihood and the normalising constant may be omitted.
 #' @examples
+#' customll_test
+#' 
 #' myll <- customll("a1type dirichlet(const veca1 &u, const veca1 &beta) {
 #'   size_t d  = u.size();
 #'   a1type y(0.);  // initialize summation at 0
@@ -43,7 +45,7 @@
 #'  myll, rep(1/3, 3), rep(NA, 3), 
 #'  bdryw="minsq", acut = 0.01)
 #' 
-#' @returns An `adloglikelood` object (which is just an `externalptr` with attributes) for the compiled log-likelihood function. The returned object has an attribute `fname`.
+#' @returns `customll()` returns an `adloglikelood` object (which is just an `externalptr` with attributes) for the compiled log-likelihood function. The returned object has an attribute `fname`.
 #' @export
 customll <- function(code, rebuild = FALSE, 
                      includes = character(),
@@ -90,7 +92,8 @@ print.adloglikelihood <- function(x, ...){
   print(sprintf("log-likelihood function '%s' for automatic differentiation", attr(x, "fname", exact = TRUE)))
 }
 
-#' @describeIn customll Returns `TRUE` if taping works in your `R` session. Otherwise it will return `FALSE` and generate warnings.
+#' @name customll
+#' @returns `customll_test()` returns `TRUE` if taping works in your `R` session. Otherwise it will return `FALSE` and generate warnings.
 #' @export
 customll_test <- function(){
   myll <- customll("a1type test(const veca1 &u, const veca1 &beta) {
