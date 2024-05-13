b9cf0de08bbeeb8c09f0adc9f25d07c6caa58b53
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-05-13 14:23:41 +1000

    No error on my linux or windows machines, but I think created an error on the win checker machine. Revert "seems like customll doesn't need to explicitly depend on RcppEigen, at least on my linux machine"
    This reverts commit bcfcbb7de7193dbb0e614b3e1dabc5b175880e3d.

--- R/customlikelihood.R
+++ R/customlikelihood.R
@@ -49,7 +49,7 @@ customll <- function(code, rebuild = FALSE,
                      includes = character(),
                      cacheDir = getOption("rcpp.cache.dir", tempdir()), 
                      showOutput = verbose, verbose = getOption("verbose")){
-  ptr <- RcppXPtrUtils::cppXPtr(code, depends = c("scorematchingad"), includes = includes, rebuild = rebuild, cacheDir = cacheDir, showOutput = showOutput, verbose = verbose)
+  ptr <- RcppXPtrUtils::cppXPtr(code, depends = c("RcppEigen", "scorematchingad"), includes = includes, rebuild = rebuild, cacheDir = cacheDir, showOutput = showOutput, verbose = verbose)
   
   # in the spirit of RcppXPtrUtils::checkXPtr check output and arguments. Rewritten here for bespoke error messages.
   if (!all(grepl("^const veca1", attr(ptr, "args", exact = TRUE)))){
