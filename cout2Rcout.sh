51dc44ac0dbe990a29b57d734ed11ccb5c24f946
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-06-13 16:47:42 +1000

    better add of cout2Rcout

--- cout2Rcout.sh
+++ cout2Rcout.sh
@@ -6,6 +6,6 @@ sed -i "s/std::cerr/Rcpp::Rcerr/g" `find inst/include/cppad/ -type f \( -iname "
 sed -i "s/ cout/ Rcout/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" \)`
 sed -i "s/ cerr/ Rcerr/g" `find inst/include/cppad/ -type f \( -iname "*.hpp" \)`
 
-# add include RcppCommon to any file that uses Rcpp
-sed -i "0,/^# include.*/s//# include <RcppCommon.h>\n&/" `grep -l "Rcpp" inst/include/cppad/* -r`
+# add include RcppCommon to any file that uses Rcpp and doesn't already have RcppCommon in it
+sed -i "0,/^# include.*/s//# include <RcppCommon.h>\n&/" `grep -L "<.*RcppCommon" $(grep -l "Rcpp" inst/include/cppad/* -r)`
 
