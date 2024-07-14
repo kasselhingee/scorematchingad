9c766875ab5d869c20b33a6c5783db3a309d65cd
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-07-14 10:56:43 +1000

    deleted a fair amount of inst/cppad, the remaining directores are needed to avoid cmake configuration errors (unless I modify these further)

--- inst/cppad/bin/build.bat
+++
@@ -1,9 +0,0 @@
-rmdir /S build
-mkdir build
-cd build
-cmake ^
-   -G "NMake Makefiles"^
-   -D cppad_cxx_flags="/MP /EHs /EHc /std:c++17 /Zc:__cplusplus"^
-   -D cppad_static_lib=FALSE^
-   ..
-nmake check
