087600c96d8d78a9c420c9309173a5334598724f
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-09-23 11:43:33 +1000

    simple size_order function exposure and void constructor exposure compiles

--- src/ADFun_module.cpp
+++ src/ADFun_module.cpp
@@ -7,12 +7,12 @@ RCPP_MODULE(cppad_module) {
 
     class_<ADFundouble>("ADFun")
         .constructor<>()
-        .method("Forward", &ADFundouble::Forward)
-        .method("Reverse", &ADFundouble::Reverse)
-        .method("Jacobian", &ADFundouble::Jacobian)
-        .method("Hessian", &ADFundouble::Hessian)
-        .method("optimize", &ADFundouble::optimize)
-        .method("size_var", &ADFundouble::size_var)
+        //.method("Forward", (std::vector<double> (ADFundouble::*)(size_t, const std::vector<double>&, std::ostream&)) &ADFundouble::Forward)
+        //.method("Reverse", &ADFundouble::Reverse)
+        //.method("Jacobian", &ADFundouble::Jacobian)
+        //.method("Hessian", &ADFundouble::Hessian)
+        //.method("optimize", &ADFundouble::optimize)
+        //.method("size_var", &ADFundouble::size_var)
         .method("size_order", &ADFundouble::size_order);
 }
 
