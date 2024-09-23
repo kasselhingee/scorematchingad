f65ed8d7ce813729ee6fdfd8724e3b53ca6ad8be
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-09-24 09:03:30 +1000

    moved definition of members of pADFun to header too - so external compilation will see it I hope

--- inst/include/utils/pADFun.h
+++ inst/include/utils/pADFun.h
@@ -1,6 +1,11 @@
 #ifndef PADFUN_H
 #define PADFUN_H
 
+//both Rcpp::wrap and Rcpp::as use copy operations, which ADFun explicitly does not have (the default is 'deleted'). [UNLESS IT WAS JUST ME ACCIDENTALLY USING COPY]?
+//So exposing pointers of ADFun objects to make interrogation possible
+//Hopefully the wrapping for Rcpp::XPtr won't override anything I try here
+
+
 #include <scorematchingad_forward.h>
 #include <Rcpp.h>
 
@@ -8,19 +13,26 @@ class pADFun {
 private:
   Rcpp::XPtr < CppAD::ADFun<double> > ptr;
 
-  static Rcpp::XPtr < CppAD::ADFun<double> > movetoXPtr(CppAD::ADFun<double> & tape);
+//MOVES the tape to a spot with memory management. The previous version of the tape will NOT be available because it this is a MOVE. Move needed because copy operation of ADFun not allowed
+Rcpp::XPtr < CppAD::ADFun<double> > movetoXPtr(CppAD::ADFun<double> & tape){
+  CppAD::ADFun<double> * out = new CppAD::ADFun<double>(std::move(tape));//reserve memory for a tape
+  Rcpp::XPtr< CppAD::ADFun<double> > pout(out, true);
+  return(pout);
+}
 
 public:
-  // Default constructor
-  pADFun();
+// Default constructor
+pADFun() : ptr(Rcpp::XPtr< CppAD::ADFun<double> >(nullptr, false)) {}
 
-  // Constructor taking tape
-  pADFun(CppAD::ADFun<double> & tape);
+// Constructor taking tape
+pADFun(CppAD::ADFun<double> & tape) : ptr(movetoXPtr(tape)) {}
 
-  // Constructor taking pADFundouble
-  pADFun(const Rcpp::XPtr<CppAD::ADFun<double>> & p);
+// Constructor taking pADFundouble
+pADFun(const Rcpp::XPtr<CppAD::ADFun<double>> & p) : ptr(p) {}
 
-  size_t size_order() const;
+size_t size_order() const {
+   return ptr->size_order();
+}
 
 };
 
--- src/pADFun.cpp
+++ src/pADFun.cpp
@@ -1,28 +1,5 @@
 #include "utils/pADFun.h"
 
-//both Rcpp::wrap and Rcpp::as use copy operations, which ADFun explicitly does not have (the default is 'deleted').
-//So exposing pointers of ADFun objects to make interrogation possible
-//Hopefully the wrapping for Rcpp::XPtr won't override anything I try here
-
-//MOVES the tape to a spot with memory management. The previous version of the tape will NOT be available because it this is a MOVE. Move needed because copy operation of ADFun not allowed
-Rcpp::XPtr < CppAD::ADFun<double> > pADFun::movetoXPtr(CppAD::ADFun<double> & tape){
-  CppAD::ADFun<double> * out = new CppAD::ADFun<double>(std::move(tape));//reserve memory for a tape
-  Rcpp::XPtr< CppAD::ADFun<double> > pout(out, true);
-  return(pout);
-}
-
-// Default constructor
-pADFun::pADFun() : ptr(Rcpp::XPtr< CppAD::ADFun<double> >(nullptr, false)) {}
-
-// Constructor taking tape
-pADFun::pADFun(CppAD::ADFun<double> & tape) : ptr(movetoXPtr(tape)) {}
-
-// Constructor taking pADFundouble
-pADFun::pADFun(const Rcpp::XPtr<CppAD::ADFun<double>> & p) : ptr(p) {}
-
-size_t pADFun::size_order() const {
-   return ptr->size_order();
-}
 
 
 RCPP_MODULE(cppad_module) {
