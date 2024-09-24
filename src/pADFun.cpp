20f129890c7261fdc46c8ce605c74ccc94a589bb
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-09-24 14:45:28 +1000

    confirmed bot tape_uld and tapell are working at the same time. Finallygit status

--- src/pADFun.cpp
+++ src/pADFun.cpp
@@ -2,7 +2,15 @@
 
 using namespace Rcpp;
 
+//small function to run that will force Module to initialise as
+//I'm seeing strange loading when using both the manifolds module
+//and the cppad_module
+void empty(){}
+
+
 RCPP_MODULE(cppad_module) {
+    function("empty", &empty);
+
     Rcpp::class_<pADFun>("ADFun")
         .constructor<Rcpp::XPtr<CppAD::ADFun<double>>>()
         .property("size_order", &pADFun::size_order, "Number of Taylor coefficient orders, per variable,direction, currently calculated and stored")
