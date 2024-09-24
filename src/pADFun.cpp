316775ff123d4c112fe075d56b8679245fd15831
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-09-24 12:27:40 +1000

    a factory and a constructor compiling

--- src/pADFun.cpp
+++ src/pADFun.cpp
@@ -2,14 +2,8 @@
 
 using namespace Rcpp;
 
-pADFun * XPtr2ppADFun(Rcpp::XPtr<CppAD::ADFun<double>> ptape){
-  pADFun * out = new pADFun(ptape);
-  return(out);
-}
-
 RCPP_MODULE(cppad_module) {
     Rcpp::class_<pADFun>("ADFun")
-        .factory<Rcpp::XPtr<CppAD::ADFun<double>>>(XPtr2ppADFun)
         .constructor<Rcpp::XPtr<CppAD::ADFun<double>>>()
         .property("size_order", &pADFun::size_order);
 }
