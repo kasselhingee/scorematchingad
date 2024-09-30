4a84a0dc36f643ef6f154a10dde726386c963e35
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-10-01 08:19:25 +1000

    tapell() tests pass

--- tests/testthat/test-tapell.R
+++ tests/testthat/test-tapell.R
@@ -1,37 +1,18 @@
-test_that("fixdynamic() reduces the dimension of the dynamic parameters", {
-maninfo <- manifoldtransform("sim", "sqrt", "sph")
-ppitape <- tapell(ll = "ppi",
-                  ytape = c(0.2, 0.3, 0.5),
-                  usertheta = ppi_paramvec(p = 3), 
-                  tranobj = maninfo$tran) 
-fixedtape <- fixdynamic(ppitape$lltape, ppitape$lltape$dyntape, ppi_paramvec(p = 3, AL = TRUE, beta = TRUE))
-})
-
 test_that("tapell generates correct objects", {
 maninfo <- manifoldtransform("sim", "sqrt", "sph")
 ppitape <- tapell(ll = "ppi",
                   ytape = c(0.2, 0.3, 0.5),
                   usertheta = ppi_paramvec(p = 3), 
-                  tranobj = maninfo$tran) 
-expect_true(R6::is.R6(ppitape))
+                  tranobj = maninfo$tran)
+expect_s4_class(ppitape, "Rcpp_ADFun")
 
-newADFun <- methods::new(cppad_module$ADFun, ppitape$ptr)
-expect_equal(newADFun$Domain, 3)
-
-# and verbose works too
-expect_output({ppitape <- tapell(ll = "ppi",
-                  ytape = c(0.2, 0.3, 0.5),
-                  usertheta = ppi_paramvec(p = 3), 
-                  tranobj = maninfo$tran,
-                  verbose = TRUE)},
-              "pattern.*tape.*dynamic")
-expect_true(R6::is.R6(ppitape))
 
 expect_error({tapell(ll = "error",
                   ytape = c(0.2, 0.3, 0.5),
                   usertheta = ppi_paramvec(p = 3), 
                   tranobj = maninfo$tran)}, "function")
 })
+
 
 test_that("tapell for ppi errors when theta isn't of the correct length", {
   maninfo <- manifoldtransform("sim", "sqrt", "sph")
@@ -47,22 +28,3 @@ test_that("tapell for ppi errors when theta isn't of the correct length", {
                   tranobj = maninfo$tran)
 })
 
-
-test_that("tapell for vMF errors if NDEBUG not defined when theta isn't of the correct length", {
-  skip("C++ assert errors not yet translated to R")
-  # should get an assert error and R aborts
-  maninfo <- manifoldtransform("sph")
-  expect_condition(vmftape <- tapell(ll = "vMF",
-                  ytape = c(0.2, 0.3, 0.5),
-                  usertheta = c(NA, NA),
-                  tranobj = maninfo$tran))
-})
-
-test_that("llptr() internal function returns points like RcppXPtrUtils", {
-  dirichletPtr <- getllptr("dirichlet")
-  expect_type(dirichletPtr, "externalptr")
-
-
-  #evaluate ppiXPtr
-  expect_equal(3 * (-0.5 * log(1/3)), evalll(dirichletPtr, rep(1/3, 3), rep(-0.5, 3)))
-})
