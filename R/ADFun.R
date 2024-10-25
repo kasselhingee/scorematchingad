29a1cead86d918a3df241605cd0ce1f066dba3ee
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-10-25 17:28:05 +1100

    Rcpp_ADFun help moved to a .R file

--- R/ADFun.R
+++ R/ADFun.R
@@ -1,38 +1,37 @@
-# R6 ADFun class. Making it here because templating of the Cpp class means it is hard to expose via Rcpp
-# Using R6 because it has modify in place semantics which match the ADFun objects
-# following https://adv-r.hadley.nz/r6.html
+# help for the Rcpp_ADFun objects exposed in src/pADFun.cpp
 
-#' @name oldADFUNdoc
-#' @title A Class for Storing a CppAD Tape (ADFun) Object
-#' @description
-#' *This is a low level object useful for implementing score matching estimators.*
-#' An `R6` class for storing a 'pointer' to a `CppAD` tape in `C++` (also called an `ADFun`) and associated information. 
-#' Currently tools for modifying this information are not available in this package, however tools for creating new `ADFun` objects from an existing `ADFun` are available.
-#' Typically an `ADFun` object will be created by [`buildsmdtape()`].
-#' @field ptr A `Rcpp` external pointer to a `CppAD` `ADFun` object.
-#' @param ptr A `Rcpp` external pointer to a `CppAD` `ADFun` object.
-#' @field xtape The (numeric) vector of independent variable values used for taping.
-#' @param xtape The (numeric) vector of independent variables used for taping.
-#' @field dyntape The (numeric) vector of dynamic parameters used for taping.
-#' @param dyntape The (numeric) vector of dynamic parameters used for taping.
-#' @field usertheta A (numeric) vector of `NA` values and fixed values specifying the parameters of taped function that were considered dynamic parameters or fixed parameters respectively.
-#' @param usertheta A (numeric) vector of `NA` values and fixed values specifying the inputs of the taped function that were considered independent variables or dynamic parameters respectively.
-#' @field name An easy to read name for the taped function
-#' @param name An easy to read name for the taped function
-#' @inheritSection buildsmdtape Introduction to CppAD Tapes
-#' @inheritSection buildsmdtape Warning: multiple CPU
-#' @examples
-#' tapes <- buildsmdtape(
-#'   "sim", "sqrt", "sph",
-#'   ll = "ppi",
-#'   ytape =  rep(1/3, 3),
-#'   usertheta = ppi_paramvec(p=3),
-#'   bdryw = "minsq",
-#'   acut = 0.01,
-#'   verbose = FALSE)
-#' tapes$smdtape$xtape
-#' tapes$smdtape$dyntape
-#' tapes$smdtape$name
-#' tapes$smdtape$ptr
+#' @name ADFun
+#' @aliases Rcpp_ADFun
+#' @title A Class That Contains CppAD Tapes
+#' @description Tapes are a record of operations performed by a function. Tapes can be evaluated, differentiated, and have properties (such as domain and range dimensions). Tapes also have dynamic parameters that can be updated. These classes uses 'reference' semantics, so that changes modify in place and copies all point to the same object (and changes modify that same object).
+#'
+#'
+#' @details `print()` will return some properties of the class. Technically the class name is 'Rcpp_ADFun' (so `inherits(x, "Rcpp_ADFun")` will return `TRUE`) and it is a reference class that connects to `CppAD` tapes in `C++`. Many of the methods available for tapes in `CppAD` are made available here.
+#'
+#' Tapes cannot be saved from session to session.
+#'
+#' # Introduction to CppAD Tapes
+#' This package uses version 2024000.5 of the algorithmic differentiation library `CppAD` \insertCite{bell2023cp}{scorematchingad} to build score matching estimators.
+#' Full help for `CppAD` can be found at <https://cppad.readthedocs.io/>.
+#' 
+#' Differentiation proceeds by *taping* the basic (*atomic*) operations performed on the independent variables and dynamic parameters. The atomic operations include multiplication, division, addition, sine, cosine, exponential and many more.
+#' Example values for the variables and parameters are used to conduct this taping, so care must be taken with any conditional (e.g. if-then) operations, and [`CppAD`](https://cppad.readthedocs.io/) has a special tool for this called `CondExp` (short for `conditional expressions`).
+#'
+#' The result of taping is an object of class `ADFun` in `CppAD` and is often called a *tape*.
+#' This `ADFun` object can be evaluated, differentiated, used for further taping (via `CppAD`'s `base2ad()`), solving differential equations and more.
+#' The differentiation is with respect to the independent variables, however the dynamic parameters can be altered which allows for creating a new `ADFun` object where the dynamic parameters become independent variables (see [`tapeSwap()`]).
+#' For the purposes of score matching, there are also *fixed* parameters, which are the elements of the model's parameter vector that are given and not estimated.
+#' 
+#' # Warning: multiple CPU
+#' Each time a tape is evaluated the corresponding `C++` object is altered. Parallel use of the same `ADFun` object thus requires care and is not tested. For now I recommend creating a new `ADFun` object for each CPU.
+#'
+#' Some further help is available by `ADFun$help()`.
+#' @param x A vector of independent variables.
+#' @param dyn A vector of dynamic parameters.
+#' @param q Differentiation order.
+#' @param i Index of range result.
+#' @field domain The number of independent variables (i.e. dimension of Euclidean domain space)
+#' @field eval Evaluation of the function at `x` given new values of the dynamic parameters `dyn`. Has two arguments, `x` and `dyn`.
+#' @export ADFun
+
 NULL
-
--- src/pADFun.cpp
+++ src/pADFun.cpp
@@ -2,38 +2,7 @@
 #include "utils/pADFun.h"
 #include <RcppEigen.h> //need RcppEigen fully included here for Rcpp to find vecd wrap and as for items below
 
-//' @name ADFun
-//' @title A Class That Contains CppAD Tapes
-//' @description Tapes are a record of operations performed by a function. Tapes can be evaluated, differentiated, and have properties (such as domain and range dimensions). Tapes also have dynamic parameters that can be updated. These classes uses 'reference' semantics, so that changes modify in place and copies all point to the same object (and changes modify that same object).
-//'
-//'
-//' @details `print()` will return some properties of the class. Technically the class name is 'Rcpp_ADFun' (so `inherits(x, "Rcpp_ADFun")` will return `TRUE`) and it is a reference class that connects to `CppAD` tapes in `C++`. Many of the methods available for tapes in `CppAD` are made available here.
-//'
-//' Tapes cannot be saved from session to session.
-//'
-//' # Introduction to CppAD Tapes
-//' This package uses version 2024000.5 of the algorithmic differentiation library `CppAD` \insertCite{bell2023cp}{scorematchingad} to build score matching estimators.
-//' Full help for `CppAD` can be found at <https://cppad.readthedocs.io/>.
-//' 
-//' Differentiation proceeds by *taping* the basic (*atomic*) operations performed on the independent variables and dynamic parameters. The atomic operations include multiplication, division, addition, sine, cosine, exponential and many more.
-//' Example values for the variables and parameters are used to conduct this taping, so care must be taken with any conditional (e.g. if-then) operations, and [`CppAD`](https://cppad.readthedocs.io/) has a special tool for this called `CondExp` (short for `conditional expressions`).
-//'
-//' The result of taping is an object of class `ADFun` in `CppAD` and is often called a *tape*.
-//' This `ADFun` object can be evaluated, differentiated, used for further taping (via `CppAD`'s `base2ad()`), solving differential equations and more.
-//' The differentiation is with respect to the independent variables, however the dynamic parameters can be altered which allows for creating a new `ADFun` object where the dynamic parameters become independent variables (see [`tapeSwap()`]).
-//' For the purposes of score matching, there are also *fixed* parameters, which are the elements of the model's parameter vector that are given and not estimated.
-//' 
-//' # Warning: multiple CPU
-//' Each time a tape is evaluated the corresponding `C++` object is altered. Parallel use of the same `ADFun` object thus requires care and is not tested. For now I recommend creating a new `ADFun` object for each CPU.
-//'
-//' Some further help is available by `ADFun$help()`.
-//' @param x A vector of independent variables.
-//' @param dyn A vector of dynamic parameters.
-//' @param q Differentiation order.
-//' @param i Index of range result.
-//' @field domain The number of independent variables (i.e. dimension of Euclidean domain space)
-//' @field eval Evaluation of the function at `x` given new values of the dynamic parameters `dyn`. Has two arguments, `x` and `dyn`.
-//' @export ADFun
+// LOOK TO R/ADFun.R FOR HELP ON THESE OBJECTS
 
 RCPP_MODULE(ADFun) {
     Rcpp::class_<pADFun>("ADFun")
