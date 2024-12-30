6f926ac43278dd340b6452ce47631c96570a0a02
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-12-30 14:21:20 +1100

    need to document print and show

--- R/ADFun_print.R
+++ R/ADFun_print.R
@@ -15,21 +15,21 @@ print.Rcpp_ADFun <- function(x, ...){
   invisible(allstr)
 }
 
-#' @noRd
-#' @describeIn Rcpp_ADFun-class Displays a summary of a \linkS4class{Rcpp_ADFun} object.
-
+#' @name print,Rcpp_ADFun
+#' @title Print or show a summary of an Rcpp_ADFun
+#' @description Both `print()` and `show()` will display a summary of a \linkS4class{Rcpp_ADFun} object.
 #' @aliases print,Rcpp_ADFun-method
 #' @param x An object of class \linkS4class{Rcpp_ADFun}.
 #' @param ... Passed to [`format()`].
-#' @importFrom methods print
 #' @export
 setMethod("print", "Rcpp_ADFun", function(x, ...){
   print.Rcpp_ADFun(x, ...)
 })
 
 # need to define show too - this the generic that happens automatically when one types object into the console - and usually it automatically print(), but I guess Rcpp has defined show() for these objects differently.
-#' @noRd
-#' @describeIn Rcpp_ADFun-class Displays a summary of a \linkS4class{Rcpp_ADFun} object and overrides the default `show()` method for \linkS4class{C++Object} objects from [`Rcpp::C++Object-class`].
+#' @name print,Rcpp_ADFun
+#' @details
+#' The `show()` method overrides the default `show()` method for \linkS4class{C++Object} objects from [`Rcpp::C++Object-class`].
 #' @aliases show,Rcpp_ADFun-method
 #' @param object An object of class \linkS4class{Rcpp_ADFun}.
 #' @importFrom methods show
