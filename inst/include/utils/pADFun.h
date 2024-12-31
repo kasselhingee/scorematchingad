bbd26ad570e7975c4feb1137650a3acccedb4884
kasselhingee <12227692+kasselhingee@users.noreply.github.com>
2024-12-31 21:21:03 +1100

    corrected order of initialisation in some constructors of pADFun

--- inst/include/utils/pADFun.h
+++ inst/include/utils/pADFun.h
@@ -27,12 +27,12 @@ pADFun() : ptr(Rcpp::XPtr< CppAD::ADFun<double> >(nullptr, false)) {}
 // Constructor taking tape
 pADFun(CppAD::ADFun<double> & tape) : ptr(movetoXPtr(tape)) {}
 pADFun(CppAD::ADFun<double> & tape, veca1 xtape, veca1 dyntape) : ptr(movetoXPtr(tape)), xtape(xtape), dyntape(dyntape) {}
-pADFun(CppAD::ADFun<double> & tape, veca1 xtape, veca1 dyntape, std::string name) : ptr(movetoXPtr(tape)), xtape(xtape), dyntape(dyntape), name(name) {}
+pADFun(CppAD::ADFun<double> & tape, veca1 xtape, veca1 dyntape, std::string name) : ptr(movetoXPtr(tape)), name(name), xtape(xtape), dyntape(dyntape) {}
 
 // Constructor taking XPtr - and copying it
 pADFun(Rcpp::XPtr<CppAD::ADFun<double>> p) : ptr(p) {}
 pADFun(Rcpp::XPtr<CppAD::ADFun<double>> p, veca1 xtape, veca1 dyntape) : ptr(p), xtape(xtape), dyntape(dyntape) {}
-pADFun(Rcpp::XPtr<CppAD::ADFun<double>> p, veca1 xtape, veca1 dyntape, std::string name) : ptr(p), xtape(xtape), dyntape(dyntape), name(name) {}
+pADFun(Rcpp::XPtr<CppAD::ADFun<double>> p, veca1 xtape, veca1 dyntape, std::string name) : ptr(p), name(name), xtape(xtape), dyntape(dyntape) {}
 
 // Properties
 size_t size_order() const { return ptr->size_order(); }
