44f308caa0cd4512155bb9eba36dd2f2ce6983d5
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-02-05 16:21:53 +1100

    drafted

--- src/fixindependent.cpp
+++ src/fixindependent.cpp
@@ -2,25 +2,25 @@
 
 pADFun fixindependent(pADFun & uld, // the unnormalised log density tape
                   veca1 x, //new x to use for taping
-                  Eigen::Matrix<int, Eigen::Dynamic, 1> fixedtheta){ //TRUE (1) values indicate that the corresponding value of theta is not a variable (dynamic or independent)
-  if (fixedtheta.size() != uld.size_dyn_ind()){
-    Rcpp::stop("fixedtheta must have the same length as the dynamic parameter vector of uld");
+                  Eigen::Matrix<int, Eigen::Dynamic, 1> fixedx){ //TRUE (1) values indicate that the corresponding value of x is not a variable (dynamic or independent)
+  if (fixedx.size() != uld.size_dyn_ind()){
+    Rcpp::stop("fixedx must have the same length as the independent vector of uld");
   }
-  if (theta.size() != fixedtheta.size()){
-    Rcpp::stop("theta and fixedtheta must have the same length");
+  if (x.size() != fixedx.size()){
+    Rcpp::stop("x and fixedx must have the same length");
   }
   
-  //separate fixed and variable theta
-  veca1 thetavar(theta.size() - fixedtheta.sum());
-  veca1 thetafxd(fixedtheta.sum());
+  //separate fixed and variable x
+  veca1 xvar(x.size() - fixedx.sum());
+  veca1 xfxd(fixedx.sum());
   size_t idx_var(0);
   size_t idx_fxd(0);
-  for (long int i=0;i<theta.size();i++){
-    if (fixedtheta[i]){
-      thetafxd[idx_fxd] = theta[i];
+  for (long int i=0;i<x.size();i++){
+    if (fixedx[i]){
+      xfxd[idx_fxd] = x[i];
       idx_fxd += 1;
     } else {
-      thetavar[idx_var] = theta[i];
+      xvar[idx_var] = x[i];
       idx_var += 1;
     }
   }
@@ -29,32 +29,32 @@ pADFun fixindependent(pADFun & uld, // the unnormalised log density tape
   CppAD::ADFun<a1type, double> uldhigher;
   uldhigher = (uld.get_ptr())->base2ad();
 
-  //redo tape with some of the dynamic parameters fixed as per fixedtheta
-  veca1 x = uld.xtape;
-  CppAD::Independent(x, thetavar);
+  //redo tape with some of the dynamic parameters fixed as per fixedx
+  veca1 theta = uld.dyntape;
+  CppAD::Independent(xvar, theta);
 
 
-  //combine fixed and variable theta
-  veca1 thetarecom(theta.size());
+  //combine fixed and variable x
+  veca1 xrecom(x.size());
   idx_var = 0;
-  for (long int i=0;i<theta.size();i++){
-    if (fixedtheta[i]){
-      thetarecom[i] = theta[i];
+  for (long int i=0;i<x.size();i++){
+    if (fixedx[i]){
+      xrecom[i] = x[i];
     } else {
-      thetarecom[i] = thetavar[idx_var];
+      xrecom[i] = xvar[idx_var];
       idx_var += 1;
     }
   }
 
-  //evaluate tape at new dynamic parameters
-  uldhigher.new_dynamic(thetarecom);
+  //evaluate tape at new values
+  uldhigher.new_dynamic(theta);
   veca1 y(uld.Range());
-  y = uldhigher.Forward(0, x);
+  y = uldhigher.Forward(0, xrecom);
   CppAD::ADFun<double> tape;  //copying the change_parameter example, a1type is used in constructing f, even though the input and outputs to f are both a2type.
   tape.Dependent(x, y);
   tape.check_for_nan(false);
 
-  pADFun out(tape, x, thetavar, uld.name);
+  pADFun out(tape, xvar, theta, uld.name);
   return out;
 }
 
