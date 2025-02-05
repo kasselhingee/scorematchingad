1b4a635f75eb7de02ed93f2a5725c7ca60cec8df
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-02-05 16:05:04 +1100

    fixdynamic help and export

--- src/fixdynamic.h
+++ src/fixdynamic.h
@@ -4,11 +4,19 @@
 #include <scorematchingad_forward.h>
 #include <utils/pADFun.h>
 
-
+//' @title Fix Dynamic Parameters of a Tape
+//' @family tape builders
+//' @param pfun An `Rcpp_ADFun` object.
+//' @param theta A numerical vector specifying the value of all dynamic parameters of `pfun`. Some of these will be fixed according to `isfixed`, the remainder will remain dynamic.
+//' @param isfixed A boolean vector same length as `theta`. `TRUE` values are fixed at the value of `theta`, `FALSE` values are left dynamic.
+//' @description Creates a tape of the function taped by `CppAD` that has some of the dynamic parameters of `pfun` fixed to specified values in `theta`.
+//' For creating this tape, the values of `pfun$xtape` is used.
+//' @return An `Rcpp_ADFun` object.
+//' @export
 // [[Rcpp::export]]
-pADFun fixdynamic(pADFun & uld, // the unnormalised log density tape
+pADFun fixdynamic(pADFun & pfun, // the unnormalised log density tape
                   veca1 theta, //new theta to use for taping
-                  Eigen::Matrix<int, Eigen::Dynamic, 1> fixedtheta); //TRUE (1) values indicate that the corresponding value of theta is not a variable (dynamic or independent)
+                  Eigen::Matrix<int, Eigen::Dynamic, 1> isfixed); //TRUE (1) values indicate that the corresponding value of theta is not a variable (dynamic or independent)
 
 // Rembed the unnormalised log-density using a different metric, which means the embedding in Euclidean space changes (so the 'x' changes too)
 // [[Rcpp::export]]
