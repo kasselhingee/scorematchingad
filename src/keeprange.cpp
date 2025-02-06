2a199b884131b98b1f60ef628e4efff57ee91f8f
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-02-06 13:34:55 +1100

    drafted keeprange()

---
+++ src/keeprange.cpp
@@ -0,0 +1,31 @@
+#include "keeprange.h"
+
+pADFun keeprange(pADFun & pfun, // the unnormalised log density tape
+                  Eigen::Matrix<int, Eigen::Dynamic, 1> keep){ //indices of range elements to keep. STARTING AT 1 FOR R COMPATABILITY 
+  if (keep.maxCoeff() > pfun.Range()){
+    Rcpp::stop("keep has indices larger than range of pfun");
+  }
+  keep = keep.array() - 1;  //convert to C++ indices
+
+  //convert taped object to execute on a1type (not double)
+  CppAD::ADFun<a1type, double> pfunhigher;
+  pfunhigher = (pfun.get_ptr())->base2ad();
+
+  //redo tape with some of the dynamic parameters fixed as per keep
+  veca1 theta = pfun.dyntape;
+  veca1 x = pfun.xtape;
+  CppAD::Independent(x, theta);
+
+  //evaluate tape at new values
+  pfunhigher.new_dynamic(theta);
+  veca1 y(pfun.Range());
+  y = pfunhigher.Forward(0, x);
+  veca1 filtered = y(keep);
+  CppAD::ADFun<double> tape;  //copying the change_parameter example, a1type is used in constructing f, even though the input and outputs to f are both a2type.
+  tape.Dependent(x, filtered);
+  tape.check_for_nan(false);
+
+  pADFun out(tape, x, theta, pfun.name);
+  return out;
+}
+
---
+++ src/keeprange.h
@@ -0,0 +1,20 @@
+#ifndef KEEPRANGE_H
+#define KEEPRANGE_H
+
+#include <scorematchingad_forward.h>
+#include <utils/pADFun.h>
+
+//' @title Reduce Range Dimension of a Tape
+//' @family tape builders
+//' @param pfun An `Rcpp_ADFun` object.
+//' @param keep Integers (lowest of 1, highest of `pfun$range()`) specifying which elements of the range to keep. To keep all pass `keep = seq(1, pfun$range())`.
+//' @description Retapes an existing `CppAD` tape omitting some of the returned elements.
+//' For creating this tape, the values of `pfun$dyntape` and `pfun$xtape` are used.
+//' @return An `Rcpp_ADFun` object.
+// [[Rcpp::export]]
+pADFun keeprange(pADFun & pfun, // the unnormalised log density tape
+                  veca1 x, //new x to use for taping
+                  Eigen::Matrix<int, Eigen::Dynamic, 1> isfixed); //TRUE (1) values indicate that the corresponding value of theta is not a variable (dynamic or independent)
+
+
+#endif
