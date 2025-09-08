2d096118cb0a6526209d1ea1be6a4a177ef971e5
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-09-09 08:55:25 +1000

    started draft of tape_smi - it needs a lot of work!

--- R/tape_smi.R
+++ R/tape_smi.R
@@ -1,16 +1,20 @@
-#' @title Build CppAD Tapes for Score Matching
+#' @title Build a Tape of the Score Matching Integrand
 #' @family tape builders
 #' @family generic score matching tools
+#' @param manifold A manifold object or name of manifold
+#' @param uld Tape of an unnormalised log-density from [`tape_uld_inbuilt()`] or [`tape_uld()`]. For convenience, if `uld` is a string, it is passed to [`tape_uld_inbuilt()`].
+#' @param transform If the `uld` is defined on a different embedding to `manifold`, then the name of the transformation that maps to the embedding of `manifold`.
+#' @param ytape Vector to use for taping the `uld`. If there is `transform`, then `ytape` is also transformed.
+#' @param usertheta Optional? A vector of parameters for taping `uld`. `NA` elements will become *dynamic parameters*. Other elements will be fixed at the provided value. This is useful for fixing some parameters of the `uld`, but the same behaviour can also be obtained with [`fixdynamic()`] and [`fixindependent()`]. The length of `usertheta` must be the correct length for `uld` and `ytape` - __no checking is conducted__.
 #' @param thetatape_creator A function that generates tape values for theta. Must take a single argument, `n` the number for values to generate.
-#' @param bdryw The name of the boundary weight function or the tape of a custom boundary weight function. See [`tape_bdryw()`]. "ones" for manifolds without boundary. For the simplex and positive orthant of the sphere, "prodsq" and "minsq" are possible - see [`ppi()`] for more information on these.
-#' @param acut An optional parameter passed to the built-in boundary weight functions. See [`ppi()`] for more information.
+#' @param bdryw The tape of a boundary weight function from [`tape_bdryw()`] or [`tape_bdryw_inbuilt()`]. For convenience use `bdryw="ones"` for manifolds without boundary. If `bdryw="prodsq"` or `bdryw="minsq"` then boundary weight function for the simplex and positive orthant of the sphere are created with default threshold - see [`ppi()`] for more information.
 #' @param verbose If `TRUE` more details are printed when taping. These details are for debugging and will likely be comprehensible only to users familiar with the source code of this package.
 #' @description
-#' For a parametric model family, the function `tape_smd()` generates `CppAD` tapes for the unnormalised log-density of the model family and of the score matching discrepancy function \eqn{A(z) + B(z) + C(z)} (defined in [`scorematchingtheory`]).
-#' Three steps are performed by `tape_smd()`: first an object that specifies the manifold and any transformation to another manifold is created; then a tape of the unnormalised log-density is created; finally a tape of \eqn{A(z) + B(z) + C(z)} is created.
+#' For a parametric model family, the function `tape_smi()` generates `CppAD` tapes for the unnormalised log-density of the model family and of the score matching integrand \eqn{A(z) + B(z) + C(z)} (defined in [`scorematchingtheory`]).
+#' Three steps are performed by `tape_smi()`: first an object that specifies the manifold and any transformation to another manifold is created; then a tape of the unnormalised log-density is created; finally a tape of \eqn{A(z) + B(z) + C(z)} is created.
 #' @details
-#' To build a tape for the score matching discrepancy function, the `scorematchingad` first tapes the map from a point \eqn{z} on the `end` manifold to the value of the unnormalised log-density, where the independent variable is the \eqn{z}, the dynamic parameter is a vector of the parameters to estimate, and the remaining model parameters are fixed and not estimated.
-#' This tape is then used to generate a tape for the score matching discrepancy function where the parameters to estimate are the independent variable.
+#' To build a tape for the score matching integrand, the `scorematchingad` first tapes the map from a point \eqn{z} on the `end` manifold to the value of the unnormalised log-density, where the independent variable is the \eqn{z}, the dynamic parameter is a vector of the parameters to estimate, and the remaining model parameters are fixed and not estimated.
+#' This tape is then used to generate a tape for the score matching integrand where the parameters to estimate are the independent variable.
 #' 
 #' The transforms of the manifold must be implemented in `C++` and selected by name.
 #' @references \insertAllCited{}
@@ -18,7 +22,7 @@
 #' @return
 #' A list of:
 #'   + an [`Rcpp_ADFun`] object containing a tape of the unnormalised log-density using the metric of the "`end`" manifold (that is the independent variable is on the `end` manifold).
-#'   + an [`Rcpp_ADFun`] object containing a tape of the score matching discrepancy function with the non-fixed parameters of the model as the independent variable, and the measurements on the `end` manifold as the dynamic parameter.
+#'   + an [`Rcpp_ADFun`] object containing a tape of the score matching integrand with the non-fixed parameters of the model as the independent variable, and the measurements on the `end` manifold as the dynamic parameter.
 #'   + some information about the tapes
 #'
 #' 
@@ -27,25 +31,25 @@
 #' u <- rep(1/sqrt(p), p)
 #' ltheta <- p #length of vMF parameter vector
 #' intheta <- rep(NA, length.out = ltheta)
-#' tapes <- tape_smd("sph", "identity", "sph", "vMF",
+#' tapes <- tape_smi("sph", "identity", "sph", "vMF",
 #'               ytape = u,
 #'               usertheta = intheta,
 #'               "ones", verbose = FALSE
 #'               )
 #' evaltape(tapes$lltape, u, runif(n = ltheta))
-#' evaltape(tapes$smdtape, runif(n = ltheta), u)
+#' evaltape(tapes$smitape, runif(n = ltheta), u)
 #' 
 #' u <- rep(1/3, 3)
-#' tapes <- tape_smd("sim", "sqrt", "sph", "ppi",
+#' tapes <- tape_smi("sim", "sqrt", "sph", "ppi",
 #'               ytape = u,
 #'               usertheta = ppi_paramvec(p = 3),
 #'               bdryw = "minsq", acut = 0.01,
 #'               verbose = FALSE
 #'               )
 #' evaltape(tapes$lltape, u, rppi_egmodel(1)$theta)
-#' evaltape(tapes$smdtape, rppi_egmodel(1)$theta, u)
+#' evaltape(tapes$smitape, rppi_egmodel(1)$theta, u)
 #' @export
-tape_smd <- function(start, tran = "identity", end = start, ll,
+tape_smi <- function(start, tran = "identity", end = start, ll,
                          ytape, usertheta,
                          bdryw = "ones", acut = 1,
                          thetatape_creator = function(n){seq(length.out = n)},
@@ -78,14 +82,14 @@ tape_smd <- function(start, tran = "identity", end = start, ll,
     stop("bdryw must be a name or a taped boundary weight function")
   }
 
-  smdtape <- tapesmd(uldtape = lltape,
+  smitape <- tapesmd(uldtape = lltape,
                         tran = tranman$tran,
                         M = tranman$man,
                         bdrywtape = bdryw,
                         verbose = verbose)
   return(list(
     lltape = lltape,
-    smdtape = smdtape,
+    smitape = smitape,
     info = list(
       transform = tran,
       endmanifold = end,
