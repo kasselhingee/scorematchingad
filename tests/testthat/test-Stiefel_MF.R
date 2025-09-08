a065556498c273f17ad15073d376f71af1406d72
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-09-08 18:30:20 +1000

    kinda drafted what I want tape_smd to look like here - in a unit test

--- tests/testthat/test-Stiefel_MF.R
+++ tests/testthat/test-Stiefel_MF.R
@@ -1,5 +1,14 @@
 test_that("estimating matrix von Mises-Fisher get close to correct", {
+  set.seed(1)
   M <- matrix(rnorm(4*2), nrow = 4, ncol = 2)
   set.seed(3)
   samp <- replicate(100, rstiefel::rmf.matrix(M))
+  
+  set.seed(4)
+  Mother <- matrix(rnorm(nrow(M)*ncol(M)),
+                   nrow = nrow(M), ncol = ncol(M))
+  tape_smd("Steifel", nrow = nrow(M), ncol = ncol(M),
+           ll = "Stiefel_MF",
+           ytape = vec(samp[,,1]),
+           usertheta = vec(Mother))
 })
